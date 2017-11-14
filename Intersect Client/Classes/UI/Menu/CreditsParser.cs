f1a1e236db6b83985ef4043520e01c1e2c5766d9
Robbie Lodico <lodico.rj@gmail.com>
2017-11-13 19:14:36 -0500

    Some minor cleanup
    Former-commit-id: 58099482c4ef888b10082173cf5df786248a07a6

--- Intersect Client/Classes/UI/Menu/CreditsParser.cs
+++ Intersect Client/Classes/UI/Menu/CreditsParser.cs
@@ -26,42 +26,39 @@ namespace Intersect.Client.Classes.UI.Menu
             {
                 //TODO Load XML from Resources
             }
-            if (xml.Trim().Length > 0)
+            if (xml.Trim().Length <= 0) return;
+            var readerSettings = new XmlReaderSettings
             {
-                XmlReaderSettings readerSettings = new XmlReaderSettings();
-                readerSettings.IgnoreWhitespace = true;
-                readerSettings.IgnoreComments = true;
-                using (XmlReader reader = XmlReader.Create(new StringReader(xml), readerSettings))
+                IgnoreWhitespace = true,
+                IgnoreComments = true
+            };
+            using (var reader = XmlReader.Create(new StringReader(xml), readerSettings))
+            {
+                while (reader.Read())
                 {
-                    while (reader.Read())
+                    if (reader.Name != "Line" || !reader.IsStartElement()) continue;
+                    CreditsLine cl;
+                    var alignment = reader.GetAttribute("alignment");
+                    switch (alignment)
                     {
-                        if (reader.Name == "Line" && reader.IsStartElement())
-                        {
-                            CreditsLine cl;
-                            var alignment = reader.GetAttribute("alignment");
-                            switch (alignment)
-                            {
-                                case "center":
-                                    cl.alignment = Alignments.CenterH;
-                                    ;
-                                    break;
-                                case "right":
-                                    cl.alignment = Alignments.Right;
-                                    break;
-                                default:
-                                    cl.alignment = Alignments.Left;
-                                    break;
-                            }
-                            cl.font = reader.GetAttribute("font");
-                            cl.size = int.Parse(reader.GetAttribute("size"));
-                            var colors = reader.GetAttribute("color").Split(',');
-                            cl.clr = new Color(int.Parse(colors[0]), int.Parse(colors[1]), int.Parse(colors[2]),
-                                int.Parse(colors[3]));
-                            reader.Read();
-                            cl.text = reader.Value;
-                            Credits.Add(cl);
-                        }
+                        case "center":
+                            cl.alignment = Alignments.CenterH;
+                            break;
+                        case "right":
+                            cl.alignment = Alignments.Right;
+                            break;
+                        default:
+                            cl.alignment = Alignments.Left;
+                            break;
                     }
+                    cl.font = reader.GetAttribute("font");
+                    cl.size = int.Parse(reader.GetAttribute("size"));
+                    var colors = reader.GetAttribute("color").Split(',');
+                    cl.clr = new Color(int.Parse(colors[0]), int.Parse(colors[1]), int.Parse(colors[2]),
+                        int.Parse(colors[3]));
+                    reader.Read();
+                    cl.text = reader.Value;
+                    Credits.Add(cl);
                 }
             }
         }
