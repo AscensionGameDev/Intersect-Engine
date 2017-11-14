9a0b110841c804ba8a3a46eeda6f7f77284e34aa
Robbie Lodico <lodico.rj@gmail.com>
2017-11-13 23:32:14 -0500

    Cleanup
    Former-commit-id: 5fc046d1c06597245ab742783d8ded10dca631cc

--- Intersect Server/Classes/Core/Main.cs
+++ Intersect Server/Classes/Core/Main.cs
@@ -1,36 +1,33 @@
 using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 
+// TODO: Move or change the namespace?
+// ReSharper disable once CheckNamespace
 namespace Intersect.Server.Classes
 {
-    public class MainClass
+    public static class MainClass
     {
         public static void Main(string[] args)
         {
             CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
             
             //Place sqlite3.dll where it's needed.
-            var dllname = "sqlite3x64.dll";
-            if (!Environment.Is64BitProcess)
+            var dllname = Environment.Is64BitProcess? "sqlite3x64.dll" : "sqlite3x86.dll";
+            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Intersect.Server.resources." + dllname))
             {
-                dllname = "sqlite3x86.dll";
-            }
-            var resources = Assembly.GetExecutingAssembly().GetManifestResourceNames();
-            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Intersect.Server.resources." + dllname))
-            {
-                using (var fs = new FileStream("sqlite3.dll", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                Debug.Assert(resourceStream != null, "resourceStream != null");
+                using (var fileStream = new FileStream("sqlite3.dll", FileMode.OpenOrCreate, FileAccess.ReadWrite))
                 {
-                    var data = new byte[stream.Length];
-                    stream.Read(data, 0, (int)stream.Length);
-                    fs.Write(data,0,data.Length);
+                    var data = new byte[resourceStream.Length];
+                    resourceStream.Read(data, 0, (int)resourceStream.Length);
+                    fileStream.Write(data,0,data.Length);
                 }
             }
-            object[] args1 = new object[1];
-            args1[0] = args;
-            Type.GetType("Intersect.Server.Classes.ServerStart").GetMethod("Start").Invoke(null, args1);
+
+            Type.GetType("Intersect.Server.Classes.ServerStart")?.GetMethod("Start")?.Invoke(null, new object[]{args});
         }
     }
 }
