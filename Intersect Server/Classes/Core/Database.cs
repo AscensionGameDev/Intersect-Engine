786475ccd49b55e8e613581f16fb4a6f8807020a
Robbie Lodico <lodico.rj@gmail.com>
2017-11-18 15:02:38 -0500

    format specifier
    Former-commit-id: 09b842d22b4bcf3797701d987ad496702b3e81a0

--- Intersect Server/Classes/Core/Database.cs
+++ Intersect Server/Classes/Core/Database.cs
@@ -240,7 +240,7 @@ namespace Intersect.Server.Classes.Core
                 OpenDatabaseConnection();
             }
             sw.Stop();
-            Log.Info($"Database backup at {DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss")} took  {sw.ElapsedMilliseconds}ms");
+            Log.Info($"Database backup at {DateTime.Now:yyyy-MM-dd hh-mm-ss} took  {sw.ElapsedMilliseconds}ms");
         }
 
         //Database setup, version checking
