efacdf53b8b61df61f2e443868c49382c8df4629
JC Snider <jcsnider3@gmail.com>
2017-11-20 21:20:59 -0500

    Fixed server not booting cross platform.
    Former-commit-id: 952018dcd198fb752a274c1fc5f9a31ea9b2a951

--- Intersect Server/Classes/Core/Main.cs
+++ Intersect Server/Classes/Core/Main.cs
@@ -1,27 +1,19 @@
 using System;
-using System.Diagnostics;
 using System.Globalization;
-using Intersect.Logging;
-using Intersect.Utilities;
 
 // TODO: Move or change the namespace?
 // ReSharper disable once CheckNamespace
 namespace Intersect.Server.Classes
 {
+    /// <summary>
+    /// Please do not modify this without JC's approval! If namespaces are referenced that are not SYSTEM.* then the server won't run cross platform.
+    /// If you want to add startup instructions see Classes/ServerStart.cs
+    /// </summary>
     public static class MainClass
     {
         public static void Main(string[] args)
         {
             CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
-            
-            //Place sqlite3.dll where it's needed.
-            var dllname = Environment.Is64BitProcess? "sqlite3x64.dll" : "sqlite3x86.dll";
-            if (!ReflectionUtils.ExtractResource($"Intersect.Server.Resources.{dllname}", "sqlite3.dll"))
-            {
-                Log.Error("Failed to extract sqlite library, terminating startup.");
-                Environment.Exit(-0x1000);
-            }
-
             Type.GetType("Intersect.Server.Classes.ServerStart")?.GetMethod("Start")?.Invoke(null, new object[]{args});
         }
     }
