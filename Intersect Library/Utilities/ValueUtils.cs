5335a3b60647526c6934a74c218c6e26e30d4f9b
Robbie Lodico <lodico.rj@gmail.com>
2018-03-03 01:12:06 -0500

    ValueUtils test
    Former-commit-id: 2ee807787aa06de2e76ca6e1e6cfdbbf719afc72

--- Intersect Library/Utilities/ValueUtils.cs
+++ Intersect Library/Utilities/ValueUtils.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Intersect.Utilities
+﻿namespace Intersect.Utilities
 {
     public static class ValueUtils
     {
