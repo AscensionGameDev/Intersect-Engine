e2eca1c313c880373c355baabbcf4d6bb2f75c80
Robbie Lodico <lodico.rj@gmail.com>
2019-02-18 00:14:33 -0500

    Weird compiler error?
    Former-commit-id: 4cd896084a5b92f82cbebf8c283738a560211a6d

--- Intersect.Server/WebApi/Modules/InfoModule.cs
+++ Intersect.Server/WebApi/Modules/InfoModule.cs
@@ -89,7 +89,7 @@ namespace Intersect.Server.WebApi.Modules
 
         private Response Get_Stats(dynamic parameters) => Response.AsJson(new
             {
-                uptime = Globals.Timing.TimeMs ?? -1,
+                uptime = Globals.Timing.TimeMs,
                 cps = Globals.Cps,
                 connectedClients = Globals.Clients?.Count,
                 onlineCount = Globals.OnlineList?.Count
