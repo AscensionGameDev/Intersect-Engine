b42832605a15ec605096ec4ce9cf22654ae201ab
Robbie Lodico <lodico.rj@gmail.com>
2019-02-18 00:21:32 -0500

    More cleanup
    Former-commit-id: a3cb154690c5e66fc67f7234261d170bf90a8ff4

--- Intersect (Core)/Logging/ConsoleOutput.cs
+++ Intersect (Core)/Logging/ConsoleOutput.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.IO;
+using JetBrains.Annotations;
 
 namespace Intersect.Logging
 {
@@ -25,41 +25,49 @@ namespace Intersect.Logging
                 ? $"{DateTime.UtcNow.ToString(TIMESTAMP_FORMAT)} [{logLevel}] {message}"
                 : $"{DateTime.UtcNow.ToString(TIMESTAMP_FORMAT)} [{logLevel}] {tag}: {message}";
 
-            if (LogLevel < LogLevel.Warn)
-                Console.Error?.WriteLine(line);
-
-            Console.Out?.WriteLine(line);
+            if (LogLevel < LogLevel.Info)
+            {
+                Console.Error.WriteLine(line);
+            }
+            else
+            {
+                Console.Out.WriteLine(line);
+            }
         }
 
-        public void Write(string tag, LogLevel logLevel, string format,
-            params object[] args)
+        public void Write(string tag, LogLevel logLevel, [NotNull] string format,
+             [NotNull] params object[] args)
             => Write(tag, logLevel, string.Format(format, args));
 
         public void Write(string tag, LogLevel logLevel, Exception exception, string message = null)
         {
             Write(tag, logLevel, $"Message: {exception?.Message}");
             Write(tag, logLevel, $"Stack Trace: {exception?.StackTrace}");
+
             if (exception?.InnerException != null)
             {
                 Write(tag, logLevel, $"Stack Trace: {exception.InnerException?.StackTrace}");
             }
+
             Write(tag, logLevel, $"Time: {DateTime.UtcNow}");
-            if (!string.IsNullOrEmpty(message)) Write(tag, logLevel, $"Note: {message}");
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                Write(tag, logLevel, $"Note: {message}");
+            }
+
             Flush();
         }
 
-        private static void EnsureOutputDirectory(string path)
+        private static void Flush()
         {
-            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-        }
-
-        private void Flush()
-        {
-            Console.Error?.Flush();
-            Console.Out?.Flush();
+            Console.Error.Flush();
+            Console.Out.Flush();
         }
 
         public void Close()
-            => Flush();
+        {
+            Flush();
+        }
     }
 }
