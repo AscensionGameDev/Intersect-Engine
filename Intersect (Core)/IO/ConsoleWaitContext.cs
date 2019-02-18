5443c5bf89a633520ca5a2367339d1737f6c12f0
Robbie Lodico <lodico.rj@gmail.com>
2019-02-18 17:52:14 -0500

    Cleaned up usings/commented out code
    Former-commit-id: fa0051885f96594f449bc49747ace4f3883de43d

--- Intersect (Core)/IO/ConsoleWaitContext.cs
+++ Intersect (Core)/IO/ConsoleWaitContext.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using JetBrains.Annotations;
 
 namespace Intersect.IO
 {
