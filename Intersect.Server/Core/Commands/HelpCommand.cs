0421dcfdf5a335eb54c5938b207eb586065eca00
Robbie Lodico <lodico.rj@gmail.com>
2019-02-25 22:31:05 -0500

    Help command & associated fixes
    Former-commit-id: 704895a82e27e54b3e6d9a7f44c6cc1da54a756b

---
+++ Intersect.Server/Core/Commands/HelpCommand.cs
@@ -0,0 +1,26 @@
+﻿using Intersect.Server.Core.CommandParsing;
+using Intersect.Server.Localization;
+
+namespace Intersect.Server.Core.Commands
+{
+    internal sealed class HelpCommand : ServerCommand
+    {
+        public HelpCommand() : base(
+            Strings.Commands.Help
+        )
+        {
+        }
+
+        protected override void HandleValue(ServerContext context, ParserResult result)
+        {
+            Console.WriteLine(@"    " + Strings.Commandoutput.helpheader);
+
+            Strings.Commands.CommandList.ForEach(command =>
+            {
+                Console.WriteLine($@"    {command?.Name,-20} - {command?.Help}");
+            });
+
+            Console.WriteLine($@"    {Strings.Commandoutput.helpfooter.ToString(Strings.Commands.commandinfo)}");
+        }
+    }
+}
