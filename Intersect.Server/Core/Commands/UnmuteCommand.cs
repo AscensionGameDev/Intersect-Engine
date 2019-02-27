a7f072936487f7a297187deabb77b896c9e4881d
Robbie Lodico <lodico.rj@gmail.com>
2019-02-26 23:10:26 -0500

    unban/unmute
    Former-commit-id: 84610e8b6894b8e2b610fb6ebac524606a305c33

---
+++ Intersect.Server/Core/Commands/UnmuteCommand.cs
@@ -0,0 +1,27 @@
+﻿using Intersect.Server.Database.PlayerData;
+using Intersect.Server.Localization;
+
+namespace Intersect.Server.Core.Commands
+{
+    internal sealed class UnmuteCommand : TargetUserCommand
+    {
+        public UnmuteCommand() : base(
+            Strings.Commands.Unmute,
+            Strings.Commands.Arguments.TargetUnmute
+        )
+        {
+        }
+
+        protected override void HandleTarget(ServerContext context, User target)
+        {
+            if (target == null)
+            {
+                Console.WriteLine($@"    {Strings.Account.notfound}");
+                return;
+            }
+
+            Mute.DeleteMute(target);
+            Console.WriteLine($@"    {Strings.Account.unmuted.ToString(target.Name)}");
+        }
+    }
+}
