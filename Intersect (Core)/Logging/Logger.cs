84475012ce6fb0a9d1d8ffceb6242359d26575a7
Robbie Lodico <lodico.rj@gmail.com>
2020-01-21 22:15:53 -0500

    Aggressive inlining for logger methods
    Former-commit-id: 65cf8fe7cd4a69019443e44a879cf13c186ad24c

--- Intersect (Core)/Logging/Logger.cs
+++ Intersect (Core)/Logging/Logger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 using JetBrains.Annotations;
 
@@ -24,6 +25,7 @@ namespace Intersect.Logging
         [NotNull]
         public LogConfiguration Configuration { get; set; }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Write(LogLevel logLevel, string message)
         {
             if (Configuration.LogLevel < logLevel)
@@ -47,6 +49,7 @@ namespace Intersect.Logging
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Write(LogLevel logLevel, string format, params object[] args)
         {
             if (Configuration.LogLevel < logLevel)
@@ -70,6 +73,7 @@ namespace Intersect.Logging
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Write(LogLevel logLevel, Exception exception, string message = null)
         {
             if (Configuration.LogLevel < logLevel)
@@ -93,111 +97,133 @@ namespace Intersect.Logging
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Write(string message)
         {
             Write(Configuration.LogLevel, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Write(string format, params object[] args)
         {
             Write(Configuration.LogLevel, format, args);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Write(Exception exception, string message = null)
         {
             Write(Configuration.LogLevel, exception, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void All(string message)
         {
             Write(LogLevel.All, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void All(string format, params object[] args)
         {
             Write(LogLevel.All, format, args);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void All(Exception exception, string message = null)
         {
             Write(LogLevel.All, exception, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Error(string message)
         {
             Write(LogLevel.Error, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Error(string format, params object[] args)
         {
             Write(LogLevel.Error, format, args);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Error(Exception exception, string message = null)
         {
             Write(LogLevel.Error, exception, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Warn(string message)
         {
             Write(LogLevel.Warn, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Warn(string format, params object[] args)
         {
             Write(LogLevel.Warn, format, args);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Warn(Exception exception, string message = null)
         {
             Write(LogLevel.Warn, exception, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Info(string message)
         {
             Write(LogLevel.Info, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Info(string format, params object[] args)
         {
             Write(LogLevel.Info, format, args);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Info(Exception exception, string message = null)
         {
             Write(LogLevel.Info, exception, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Trace(string message)
         {
             Write(LogLevel.Trace, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Trace(string format, params object[] args)
         {
             Write(LogLevel.Trace, format, args);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Trace(Exception exception, string message = null)
         {
             Write(LogLevel.Trace, exception, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Debug(string message)
         {
             Write(LogLevel.Debug, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Debug(string format, params object[] args)
         {
             Write(LogLevel.Debug, format, args);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Debug(Exception exception, string message = null)
         {
             Write(LogLevel.Debug, exception, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Diagnostic(string message)
         {
 #if INTERSECT_DIAGNOSTIC
@@ -205,6 +231,7 @@ namespace Intersect.Logging
 #endif
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Diagnostic(string format, params object[] args)
         {
 #if INTERSECT_DIAGNOSTIC
@@ -212,6 +239,7 @@ namespace Intersect.Logging
 #endif
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Diagnostic(Exception exception, string message = null)
         {
 #if INTERSECT_DIAGNOSTIC
@@ -219,16 +247,19 @@ namespace Intersect.Logging
 #endif
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Verbose(string message)
         {
             Write(LogLevel.Verbose, message);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Verbose(string format, params object[] args)
         {
             Write(LogLevel.Verbose, format, args);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public virtual void Verbose(Exception exception, string message = null)
         {
             Write(LogLevel.Verbose, exception, message);
