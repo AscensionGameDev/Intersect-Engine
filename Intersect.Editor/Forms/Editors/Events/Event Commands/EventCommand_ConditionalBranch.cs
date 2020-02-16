887c56645bd3f1246655d2d7a32edc6cd1c05845
JC Snider <jcsnider3@gmail.com>
2020-02-16 17:11:18 -0500

    Fix for #150

--- Intersect.Editor/Forms/Editors/Events/Event Commands/EventCommand_ConditionalBranch.cs
+++ Intersect.Editor/Forms/Editors/Events/Event Commands/EventCommand_ConditionalBranch.cs
@@ -953,5 +953,11 @@ namespace Intersect.Editor.Forms.Editors.Events.Event_Commands
 
 
         #endregion
+
+        private void lblStringTextVariables_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(
+                "http://www.ascensiongamedev.com/community/topic/749-event-text-variables/");
+        }
     }
 }
