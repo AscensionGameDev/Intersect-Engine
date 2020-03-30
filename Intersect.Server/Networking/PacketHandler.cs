5ef2b5fb567d129ea6533d5ea8e779525620b748
JC Snider <jcsnider@users.noreply.github.com>
2020-03-30 10:18:52 -0400

    Fixing bugs (#47) (#48)
    * Should fix 198, server crash due to projectile trying to hit an event
    
    * Harden accept trade packet to return if counterparty is null.
    
    * Fixing potential NREs
    
    * Fixes another potential NRE and out of bounds error
    
    * Don't want to handle packets for null clients, but also don't want to be throwing exceptions
    
    * Fixes a small error where empty passwords crash the server during migration

--- Intersect.Server/Networking/PacketHandler.cs
+++ Intersect.Server/Networking/PacketHandler.cs
@@ -39,7 +39,7 @@ namespace Intersect.Server.Networking
             var client = Client.FindBeta4Client(connection);
             if (client == null)
             {
-                throw new Exception("Client is null!");
+                return false;
             }
 
             if (client.Banned || client.FloodKicked)
@@ -1646,7 +1646,7 @@ namespace Intersect.Server.Networking
         //AcceptTradePacket
         public void HandlePacket(Client client, Player player, AcceptTradePacket packet)
         {
-            if (player == null)
+            if (player == null || player.Trading.Counterparty == null)
             {
                 return;
             }
@@ -1866,6 +1866,7 @@ namespace Intersect.Server.Networking
         //SelectCharacterPacket
         public void HandlePacket(Client client, Player player, SelectCharacterPacket packet)
         {
+            if (client.User == null) return;
             var character = DbInterface.GetUserCharacter(client.User, packet.CharacterId);
             if (character != null)
             {
@@ -1887,6 +1888,7 @@ namespace Intersect.Server.Networking
         //DeleteCharacterPacket
         public void HandlePacket(Client client, Player player, DeleteCharacterPacket packet)
         {
+            if (client.User == null) return;
             var character = DbInterface.GetUserCharacter(client.User, packet.CharacterId);
             if (character != null)
             {
