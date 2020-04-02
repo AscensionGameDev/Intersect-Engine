f7049f6dd39e29f073ae68dfada139cb2fa9d491
rhathaway86 <56697566+rhathaway86@users.noreply.github.com>
2020-04-02 14:33:37 -0500

    Added Database Migration

---
+++ Intersect.Server/Migrations/Game/20200402190906_DirectionalAnimationWeaponsToItems.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Intersect.Server.Migrations.Game
+{
+    public partial class DirectionalAnimationWeaponsToItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AttackAnimationDown",
+                table: "Items",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AttackAnimationLeft",
+                table: "Items",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AttackAnimationRight",
+                table: "Items",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AttackAnimationUp",
+                table: "Items",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DirectionalAnimation",
+                table: "Items",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttackAnimationDown",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "AttackAnimationLeft",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "AttackAnimationRight",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "AttackAnimationUp",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "DirectionalAnimation",
+                table: "Items");
+        }
+    }
+}
