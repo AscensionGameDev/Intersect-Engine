d1613217d8241da4c5c509050f0f5c22c48466fd
Bandana <aurelende@protonmail.com>
2020-04-03 17:28:51 -0400

    offensive npc move diagonal
    Signed-off-by: Bandana <aurelende@protonmail.com>
    Signed-off-by: TheBunnyGamer <65855333+TheBunnyGamer@users.noreply.github.com>

--- Intersect.Server/Entities/Pathfinding/Pathfinder.cs
+++ Intersect.Server/Entities/Pathfinding/Pathfinder.cs
@@ -447,29 +447,53 @@ namespace Intersect.Server.Entities.Pathfinding
                     {
                         var newX = enm.Current.X - Options.MapWidth;
                         var newY = enm.Current.Y - Options.MapHeight;
-                        if (mEntity.X < newX)
+                        if (mEntity.X < newX && mEntity.Y == newY)
                         {
                             enm.Dispose();
 
                             return (int) Directions.Right;
                         }
-                        else if (mEntity.X > newX)
+                        else if (mEntity.X > newX && mEntity.Y == newY)
                         {
                             enm.Dispose();
 
                             return (int) Directions.Left;
                         }
-                        else if (mEntity.Y < newY)
+                        else if (mEntity.Y < newY && mEntity.X == newX)
                         {
                             enm.Dispose();
 
                             return (int) Directions.Down;
                         }
-                        else if (mEntity.Y > newY)
+                        else if (mEntity.Y > newY && mEntity.X == newX)
                         {
                             enm.Dispose();
 
                             return (int) Directions.Up;
+                        }
+                        else if (mEntity.Y > newY && mEntity.X > newX)
+                        {
+                            enm.Dispose();
+
+                            return (int)Directions.UpLeft;
+                        }
+                        else if (mEntity.Y > newY && mEntity.X < newX)
+                        {
+                            enm.Dispose();
+
+                            return (int)Directions.UpRight;
+                        }
+                        else if (mEntity.Y < newY && mEntity.X > newX)
+                        {
+                            enm.Dispose();
+
+                            return (int)Directions.DownLeft;
+                        }
+                        else if (mEntity.Y < newY && mEntity.X < newX)
+                        {
+                            enm.Dispose();
+
+                            return (int)Directions.DownRight;
                         }
                     }
                 }
