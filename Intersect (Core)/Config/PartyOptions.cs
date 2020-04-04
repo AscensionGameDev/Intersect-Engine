cb1d854ba4434456e7fff10a29112cc803ba5201
JC Snider <jcsnider3@gmail.com>
2020-04-03 23:23:44 -0400

    Added in enhanced party options so you can configure how close players need to be to get xp and for the npc death party common events to run

---
+++ Intersect (Core)/Config/PartyOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intersect.Config
+{
+    public class PartyOptions
+    {
+
+        public int InviteRange = 40;
+
+        public int SharedXpRange = 40;
+
+        public int NpcDeathCommonEventStartRange = 0;
+
+    }
+}
