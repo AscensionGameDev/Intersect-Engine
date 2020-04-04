3072ff3a01d8b83625b2dcc9a1d16869e059522f
Bandana <aurelende@protonmail.com>
2020-04-03 20:12:38 -0400

    Diagonal attack
    Signed-off-by: Bandana <aurelende@protonmail.com>
    Signed-off-by: Eroyuu <ZeroOnline@users.noreply.github.com>

--- Intersect.Server/Networking/PacketHandler.cs
+++ Intersect.Server/Networking/PacketHandler.cs
@@ -789,6 +789,22 @@ namespace Intersect.Server.Networking
                     attackingTile.Translate(1, 0);
 
                     break;
+                case 4:
+                    attackingTile.Translate(-1, -1); // UpLeft
+
+                    break;
+                case 5:
+                    attackingTile.Translate(1, -1); // UpRight
+
+                    break;
+                case 6:
+                    attackingTile.Translate(-1, 1); // DownLeft
+
+                    break;
+                case 7:
+                    attackingTile.Translate(1, 1); // DownRight
+
+                    break;
             }
 
             PacketSender.SendEntityAttack(player, player.CalculateAttackTime());
