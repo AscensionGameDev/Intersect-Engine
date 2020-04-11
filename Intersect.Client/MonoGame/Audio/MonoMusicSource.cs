4f8775749ffb4e112a02c1242b7e267af327a4ec
JC Snider <jcsnider@users.noreply.github.com>
2020-04-11 19:05:13 -0400

    Upgrading monogame to dev (#119)
    * Client MonoGame upgrade to 3.8.0.1217-develop  which includes audio fixes
    
    * Revert "Use dynamicsoundeffects for music instead of mediaplayer (#51)"
    
    This reverts commit 3bfef746233ce5014bf0f0db3198877bd6013d49.

--- Intersect.Client/MonoGame/Audio/MonoMusicSource.cs
+++ Intersect.Client/MonoGame/Audio/MonoMusicSource.cs
@@ -3,13 +3,9 @@
 using Intersect.Client.Framework.Audio;
 using Intersect.Client.Interface.Game.Chat;
 using Intersect.Client.Localization;
-using Intersect.Client.Utilities;
 using Intersect.Logging;
 
-using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Media;
-
-using NVorbis;
 
 namespace Intersect.Client.MonoGame.Audio
 {
@@ -18,10 +14,6 @@ namespace Intersect.Client.MonoGame.Audio
     {
 
         private readonly string mPath;
-
-        private VorbisReader mReader;
-
-        private DynamicSoundEffectInstance mInstance;
 
         public MonoMusicSource(string path)
         {
@@ -33,28 +25,13 @@ namespace Intersect.Client.MonoGame.Audio
             return new MonoMusicInstance(this);
         }
 
-        public DynamicSoundEffectInstance LoadSong()
+        public Song LoadSong()
         {
             if (!string.IsNullOrWhiteSpace(mPath))
             {
                 try
                 {
-                    if (mReader == null)
-                    {
-                        mReader = new VorbisReader(mPath);
-                    }
-
-                    if (mInstance != null)
-                    {
-                        mInstance.Dispose();
-                        mInstance = null;
-                    }
-
-                    mInstance = new DynamicSoundEffectInstance(mReader.SampleRate, mReader.Channels == 1 ? AudioChannels.Mono : AudioChannels.Stereo);
-                    mInstance.BufferNeeded += BufferNeeded;
-
-
-                    return mInstance;
+                    return Song.FromUri(mPath, new Uri(mPath, UriKind.Relative));
                 }
                 catch (Exception exception)
                 {
@@ -68,47 +45,6 @@ namespace Intersect.Client.MonoGame.Audio
             }
 
             return null;
-        }
-
-        public void Close()
-        {
-            if (mReader != null)
-            {
-                mReader.Dispose();
-                mReader = null;
-            }
-        }
-
-        private void BufferNeeded(object sender, EventArgs args)
-            => FillBuffers();
-
-        private void FillBuffers(int buffers = 3, int samples = 44100)
-        {
-            float[] sampleBuffer = null;
-
-            while (mInstance.PendingBufferCount < buffers && mReader != null)
-            {
-                if (sampleBuffer == null)
-                    sampleBuffer = new float[samples];
-
-                var read = mReader.ReadSamples(sampleBuffer, 0, sampleBuffer.Length);
-                if (read == 0)
-                {
-                    mReader.DecodedPosition = 0;
-                    continue;
-                }
-
-                var dataBuffer = new byte[read << 1];
-                for (var sampleIndex = 0; sampleIndex < read; ++sampleIndex)
-                {
-                    var sample = (short)MathHelper.Clamp(sampleBuffer[sampleIndex] * 32767f, short.MinValue, short.MaxValue);
-                    var sampleData = BitConverter.GetBytes(sample);
-                    for (var sampleByteIndex = 0; sampleByteIndex < sampleData.Length; ++sampleByteIndex)
-                        dataBuffer[(sampleIndex << 1) + sampleByteIndex] = sampleData[sampleByteIndex];
-                }
-
-                mInstance.SubmitBuffer(dataBuffer, 0, read << 1);
-            }
         }
 
     }
