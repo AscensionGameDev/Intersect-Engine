1f53a16d6ac96fcda5167a1a34e258e330195b53
Bandana <aurelende@protonmail.com>
2020-04-14 10:35:29 -0400

    diag knockback fix
    Signed-off-by: TheBunnyGamer <65855333+TheBunnyGamer@users.noreply.github.com>

--- Intersect.Server/Entities/Entity.cs
+++ Intersect.Server/Entities/Entity.cs
@@ -1445,7 +1445,8 @@ namespace Intersect.Server.Entities
             }
 
             //If there is a knockback, knock them backwards and make sure its linear (diagonal player movement not coded).
-            if (projectile.Knockback > 0 && projectileDir < 4)
+            //Diagonal movement coded with diagonal patch
+            if (projectile.Knockback > 0 && projectileDir < 8)
             {
                 var dash = new Dash(target, projectile.Knockback, projectileDir, false, false, false, false);
             }
