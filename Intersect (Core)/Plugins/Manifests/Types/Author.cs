b6b682038194da2427e1b36b3221156318961a64
Robert Lodico <lodico.rj@gmail.com>
2020-05-03 22:06:08 -0400

    Author and Authors structs

---
+++ Intersect (Core)/Plugins/Manifests/Types/Author.cs
@@ -0,0 +1,226 @@
+﻿using Intersect.Utilities;
+
+using JetBrains.Annotations;
+
+using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace Intersect.Plugins.Manifests.Types
+{
+    /// <summary>
+    /// Structure that defines an <see cref="Author"/> that can be represented as an <see cref="Author"/> <see cref="string"/>.
+    /// </summary>
+    [JsonConverter(typeof(AuthorStringConverter))]
+    public struct Author : IComparable<Author>, IEquatable<Author>, IEquatable<string>, ICloneable, IComparable
+    {
+        #region Constants
+
+        /// <summary>
+        /// Expression that defines what qualifies as an <see cref="Author"/> <see cref="string"/>.
+        ///
+        /// Can be summarized as <code>Name[ &lt;Email&gt;][ (Homepage)]</code>
+        /// </summary>
+        [NotNull]
+        public static readonly Regex AuthorStringExpression =
+            new Regex(@"^\s*(.+?)(?:\s+<([^>]+)>)?(?:\s+\(([^\\)]+)\))?\s*$");
+
+        /// <summary>
+        /// The empty <see cref="Author"/> created using the default constructor.
+        /// </summary>
+        public static Author Empty => new Author();
+
+        #endregion Constants
+
+        #region Fields
+
+        /// <summary>
+        /// Email address of the <see cref="Author"/> (not validated).
+        /// </summary>
+        [NotNull]
+        public readonly string Email;
+
+        /// <summary>
+        /// Homepage (URL) of the <see cref="Author"/> (not validated).
+        /// </summary>
+        [NotNull]
+        public readonly string Homepage;
+
+        /// <summary>
+        /// Name of the <see cref="Author"/>.
+        /// </summary>
+        [NotNull]
+        public readonly string Name;
+
+        /// <summary>
+        /// Precomputed hash code.
+        /// </summary>
+        private readonly int mHashCode;
+
+        #endregion Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructs an <see cref="Author"/> from a <see cref="string"/>.
+        /// </summary>
+        /// <param name="authorString">an <see cref="Author"/> <see cref="string"/></param>
+        /// <exception cref="ArgumentNullException">thrown if <paramref name="authorString"/> is null, empty, or whitespace</exception>
+        /// <exception cref="ArgumentException">thrown if <paramref name="authorString"/> does not match the format defined by <see cref="AuthorStringExpression" /></exception>
+        /// <seealso cref="AuthorStringExpression"/>
+        public Author([CanBeNull] string authorString)
+        {
+            if (string.IsNullOrWhiteSpace(authorString))
+            {
+                throw new ArgumentNullException(nameof(authorString), @"Author string is null or whitespace.");
+            }
+
+            var match = AuthorStringExpression.Match(authorString);
+            var name = match.Groups[1];
+            var email = match.Groups[2];
+            var homepage = match.Groups[3];
+
+            Name = name.Value;
+            Email = email.Value;
+            Homepage = homepage.Value;
+
+            mHashCode = ValueUtils.ComputeHashCode(Name, Email, Homepage);
+        }
+
+        /// <summary>
+        /// Constructs an <see cref="Author"/> from an explicitly defined name, email, and homepage.
+        /// </summary>
+        /// <param name="name">the name of the <see cref="Author"/></param>
+        /// <param name="email">the email address of the <see cref="Author"/></param>
+        /// <param name="homepage">the homepage of the <see cref="Author"/></param>
+        [JsonConstructor]
+        public Author([NotNull] string name, [NotNull] string email, [CanBeNull] string homepage = null)
+        {
+            Name = name;
+            Email = email;
+            Homepage = homepage ?? string.Empty;
+
+            mHashCode = ValueUtils.ComputeHashCode(Name, Email, Homepage);
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the parts as an enumerable for <see cref="CompareTo(Author)"/>.
+        /// </summary>
+        [NotNull]
+        private IEnumerable<string> Parts => new[] {Name, Email, Homepage};
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <inheritdoc cref="ICloneable.Clone"/>
+        public Author Clone() => new Author(Name, Email, Homepage);
+
+        object ICloneable.Clone() => Clone();
+
+        /// <inheritdoc />
+        public int CompareTo(object obj) =>
+            obj is Author other
+                ? CompareTo(other)
+                : throw new NotSupportedException("Comparison only supported with Author.");
+
+        /// <inheritdoc />
+        public int CompareTo(Author other) => ValueUtils.Compare(Parts, other.Parts);
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is Author author && Equals(author);
+
+        /// <inheritdoc />
+        public bool Equals(Author other) => this == other;
+
+        /// <inheritdoc />
+        public bool Equals(string other) => this == other;
+
+        /// <inheritdoc />
+        public override int GetHashCode() => mHashCode;
+
+        /// <inheritdoc />
+        public override string ToString() =>
+            $"{Name}{(string.IsNullOrEmpty(Email) ? "" : $" <{Email}>")}{(string.IsNullOrEmpty(Homepage) ? "" : $" ({Homepage})")}";
+
+        #endregion Methods
+
+        #region Operators
+
+        /// <summary>
+        /// Converts a <see cref="string"/> to an <see cref="Author"/>.
+        /// </summary>
+        /// <param name="authorString">an <see cref="Author"/> <see cref="string"/></param>
+        public static implicit operator Author([CanBeNull] string authorString) =>
+            authorString == null ? Empty : new Author(authorString);
+
+        /// <summary>
+        /// Converts an <see cref="Author"/> to a <see cref="string"/>.
+        /// </summary>
+        /// <param name="author">an <see cref="Author"/></param>
+        public static implicit operator string(Author author) => author.ToString();
+
+        /// <summary>
+        /// Checks if two <see cref="Author"/>s are not equal.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>if any of the parts are not equal</returns>
+        public static bool operator !=(Author a, Author b) =>
+            !string.Equals(a.Name, b.Name, StringComparison.Ordinal) ||
+            !string.Equals(a.Email, b.Email, StringComparison.Ordinal) ||
+            !string.Equals(a.Homepage, b.Homepage, StringComparison.Ordinal);
+
+        /// <summary>
+        /// Checks if two <see cref="Author"/>s are equal.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>if all of the parts are equal</returns>
+        public static bool operator ==(Author a, Author b) =>
+            string.Equals(a.Name, b.Name, StringComparison.Ordinal) &&
+            string.Equals(a.Email, b.Email, StringComparison.Ordinal) &&
+            string.Equals(a.Homepage, b.Homepage, StringComparison.Ordinal);
+
+        #endregion Operators
+
+        #region Classes
+
+        private class AuthorStringConverter : JsonConverter<Author>
+        {
+            #region Properties
+
+            public override bool CanRead => false;
+
+            #endregion Properties
+
+            #region Methods
+
+            /// <inheritdoc />
+            [ExcludeFromCodeCoverage] // Will not be used because CanRead is false
+            public override Author ReadJson(
+                JsonReader reader,
+                Type objectType,
+                Author existingValue,
+                bool hasExistingValue,
+                JsonSerializer serializer
+            ) =>
+                throw new NotImplementedException();
+
+            /// <inheritdoc />
+            public override void WriteJson(JsonWriter writer, Author value, JsonSerializer serializer) =>
+                writer?.WriteValue(value.ToString());
+
+            #endregion Methods
+        }
+
+        #endregion Classes
+    }
+}
---
+++ Intersect.Tests/Plugins/Manifests/Types/AuthorTests.cs
@@ -0,0 +1,322 @@
+﻿using Intersect.Utilities;
+
+using NUnit.Framework;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Intersect.Plugins.Manifests.Types
+{
+    [TestFixture]
+    public class AuthorTests
+    {
+        #region Test Constants
+
+        private const string AuthorEmail = "test@email.com";
+
+        private const string AuthorHomepage = "http://example.com";
+
+        private const string AuthorName = "Test Author";
+
+        private static readonly string AuthorStringNameEmail = $"{AuthorName} <{AuthorEmail}>";
+
+        private static readonly string AuthorStringNameEmailHomepage =
+            $"{AuthorName} <{AuthorEmail}> ({AuthorHomepage})";
+
+        private static readonly string AuthorStringNameHomepage = $"{AuthorName} ({AuthorHomepage})";
+
+        #endregion Test Constants
+
+        #region Tests
+
+        [Test]
+        public void AreEqual_Operator()
+        {
+            var author = new Author(AuthorStringNameEmailHomepage);
+            Assert.IsTrue(author == new Author(AuthorStringNameEmailHomepage));
+        }
+
+        [Test]
+        public void AreNotEqual_Operator()
+        {
+            var author = new Author(AuthorStringNameEmailHomepage);
+            Assert.IsTrue(author != new Author(AuthorName));
+        }
+
+        [Test]
+        public void AuthorFromParts()
+        {
+            var author = new Author(AuthorName, AuthorEmail, AuthorHomepage);
+            Assert.AreEqual(AuthorName, author.Name);
+            Assert.AreEqual(AuthorEmail, author.Email);
+            Assert.AreEqual(AuthorHomepage, author.Homepage);
+        }
+
+        [Test]
+        public void AuthorFromPartsWithoutHomepage()
+        {
+            var author = new Author(AuthorName, AuthorEmail);
+            Assert.AreEqual(AuthorName, author.Name);
+            Assert.AreEqual(AuthorEmail, author.Email);
+            Assert.IsEmpty(author.Homepage);
+        }
+
+        [Test]
+        public void AuthorPaddedWhitespace()
+        {
+            var author = new Author($" \t {AuthorName} \t <{AuthorEmail}> \t ({AuthorHomepage}) \t ");
+            Assert.AreEqual(AuthorName, author.Name);
+            Assert.AreEqual(AuthorEmail, author.Email);
+            Assert.AreEqual(AuthorHomepage, author.Homepage);
+        }
+
+        [Test]
+        public void AuthorWithEmail()
+        {
+            var author = new Author(AuthorStringNameEmail);
+            Assert.AreEqual(AuthorName, author.Name);
+            Assert.AreEqual(AuthorEmail, author.Email);
+            Assert.IsEmpty(author.Homepage);
+        }
+
+        [Test]
+        public void AuthorWithEmailAndHomepage()
+        {
+            var author = new Author(AuthorStringNameEmailHomepage);
+            Assert.AreEqual(AuthorName, author.Name);
+            Assert.AreEqual(AuthorEmail, author.Email);
+            Assert.AreEqual(AuthorHomepage, author.Homepage);
+        }
+
+        [Test]
+        public void AuthorWithHomepage()
+        {
+            var author = new Author(AuthorStringNameHomepage);
+            Assert.AreEqual(AuthorName, author.Name);
+            Assert.IsEmpty(author.Email);
+            Assert.AreEqual(AuthorHomepage, author.Homepage);
+        }
+
+        [Test]
+        public void BasicAuthor()
+        {
+            var author = new Author(AuthorName);
+            Assert.AreEqual(AuthorName, author.Name);
+            Assert.IsEmpty(author.Email);
+            Assert.IsEmpty(author.Homepage);
+        }
+
+        [Test]
+        public void Clone()
+        {
+            var author = new Author(AuthorName, AuthorEmail, AuthorHomepage);
+            var clone = author.Clone();
+            Assert.AreEqual(AuthorName, clone.Name);
+            Assert.AreEqual(AuthorEmail, clone.Email);
+            Assert.AreEqual(AuthorHomepage, clone.Homepage);
+        }
+
+        [Test]
+        public void CompareTo_Name_NameEmailHomepage()
+        {
+            var author = new Author(AuthorName);
+            Assert.AreEqual(-1, author.CompareTo(AuthorStringNameEmailHomepage));
+        }
+
+        [Test]
+        public void CompareTo_NameEmailHomepage_NameEmailHomepage()
+        {
+            var author = new Author(AuthorStringNameEmailHomepage);
+            Assert.AreEqual(0, author.CompareTo(AuthorStringNameEmailHomepage));
+        }
+
+        [Test]
+        public void CompareTo_Null()
+        {
+            var author = new Author(AuthorName);
+            Assert.AreEqual(1, author.CompareTo(null));
+        }
+
+        [Test]
+        public void CompareTo_Object()
+        {
+            var author = new Author(AuthorName);
+
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+            Assert.Throws(
+                typeof(NotSupportedException), () => author.CompareTo(new object()),
+                "Comparison only supported with Author."
+            );
+        }
+
+        [Test]
+        public void Equals_Author_Equal()
+        {
+            var author = new Author(AuthorStringNameEmailHomepage);
+            Assert.IsTrue(author.Equals(new Author(AuthorStringNameEmailHomepage)));
+        }
+
+        [Test]
+        public void Equals_Author_NotEqual()
+        {
+            var author = new Author(AuthorStringNameEmailHomepage);
+            Assert.IsFalse(author.Equals(new Author(AuthorName)));
+        }
+
+        [Test]
+        public void Equals_Null()
+        {
+            var author = new Author(AuthorStringNameEmailHomepage);
+            Assert.IsFalse(author.Equals((object) null));
+        }
+
+        [Test]
+        public void Equals_Null_Operator()
+        {
+            var author = new Author(AuthorStringNameEmailHomepage);
+            Assert.IsFalse(author == null);
+        }
+
+        [Test]
+        public void Equals_Object()
+        {
+            var author = new Author(AuthorStringNameEmailHomepage);
+            Assert.IsFalse(author.Equals(new object()));
+        }
+
+        [Test]
+        public void HashCode_AuthorString_Name()
+        {
+            var author = new Author(AuthorName);
+            var hashCode = ValueUtils.ComputeHashCode(AuthorName, string.Empty, string.Empty);
+            Assert.AreEqual(hashCode, author.GetHashCode());
+        }
+
+        [Test]
+        public void HashCode_AuthorString_NameEmail()
+        {
+            var author = new Author(AuthorStringNameEmail);
+            var hashCode = ValueUtils.ComputeHashCode(AuthorName, AuthorEmail, string.Empty);
+            Assert.AreEqual(hashCode, author.GetHashCode());
+        }
+
+        [Test]
+        public void HashCode_AuthorString_NameEmailHomepage()
+        {
+            var author = new Author(AuthorStringNameEmailHomepage);
+            var hashCode = ValueUtils.ComputeHashCode(AuthorName, AuthorEmail, AuthorHomepage);
+            Assert.AreEqual(hashCode, author.GetHashCode());
+        }
+
+        [Test]
+        public void HashCode_AuthorString_NameHomepage()
+        {
+            var author = new Author(AuthorStringNameHomepage);
+            var hashCode = ValueUtils.ComputeHashCode(AuthorName, string.Empty, AuthorHomepage);
+            Assert.AreEqual(hashCode, author.GetHashCode());
+        }
+
+        [Test]
+        public void HashCode_Parts()
+        {
+            var author = new Author(AuthorName, AuthorEmail, AuthorHomepage);
+            var hashCode = ValueUtils.ComputeHashCode(AuthorName, AuthorEmail, AuthorHomepage);
+            Assert.AreEqual(hashCode, author.GetHashCode());
+        }
+
+        [Test]
+        public void HashCode_Parts_WithoutHomepage()
+        {
+            var author = new Author(AuthorName, AuthorEmail);
+            var hashCode = ValueUtils.ComputeHashCode(AuthorName, AuthorEmail, string.Empty);
+            Assert.AreEqual(hashCode, author.GetHashCode());
+        }
+
+        [Test]
+        public void ICloneable_Clone()
+        {
+            ICloneable cloneable = new Author(AuthorName, AuthorEmail, AuthorHomepage);
+            var clone = cloneable.Clone();
+            if (clone is Author author)
+            {
+                Assert.AreEqual(AuthorName, author.Name);
+                Assert.AreEqual(AuthorEmail, author.Email);
+                Assert.AreEqual(AuthorHomepage, author.Homepage);
+            }
+            else
+            {
+                Assert.Fail("Clone is not an author.");
+            }
+        }
+
+        [Test]
+        [SuppressMessage(
+            "Style", "IDE0022:Use expression body for methods",
+            Justification = "Tests should have a proper method body."
+        )]
+        public void InvalidAuthorEmpty()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.Throws(typeof(ArgumentNullException), () => new Author(""), "Author string is null or whitespace.");
+        }
+
+        [Test]
+        [SuppressMessage(
+            "Style", "IDE0022:Use expression body for methods",
+            Justification = "Tests should have a proper method body."
+        )]
+        public void InvalidAuthorNull()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.Throws(
+                typeof(ArgumentNullException), () => new Author(null), "Author string is null or whitespace."
+            );
+        }
+
+        [Test]
+        public void Operator_ImplicitFromString()
+        {
+            Author author = AuthorStringNameEmailHomepage;
+            Assert.AreEqual(AuthorName, author.Name);
+            Assert.AreEqual(AuthorEmail, author.Email);
+            Assert.AreEqual(AuthorHomepage, author.Homepage);
+        }
+
+        [Test]
+        public void Operator_ImplicitToString()
+        {
+            string authorString = new Author(AuthorName, AuthorEmail, AuthorHomepage);
+            Assert.AreEqual(AuthorStringNameEmailHomepage, authorString);
+        }
+
+        [Test]
+        public void ToString_Name()
+        {
+            var author = new Author(AuthorName);
+            Assert.AreEqual(AuthorName, author.ToString());
+        }
+
+        [Test]
+        public void ToString_NameEmail()
+        {
+            var author = new Author(AuthorName, AuthorEmail);
+            Assert.AreEqual(AuthorStringNameEmail, author.ToString());
+        }
+
+        [Test]
+        public void ToString_NameEmailHomepage()
+        {
+            var author = new Author(AuthorName, AuthorEmail, AuthorHomepage);
+            Assert.AreEqual(AuthorStringNameEmailHomepage, author.ToString());
+        }
+
+        [Test]
+        public void ToString_NameHomepage()
+        {
+            var author = new Author(AuthorName, string.Empty, AuthorHomepage);
+            Assert.AreEqual(AuthorStringNameHomepage, author.ToString());
+        }
+
+        #endregion Tests
+    }
+}
