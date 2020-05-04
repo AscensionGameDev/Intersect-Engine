c0a92bb2900c452a01412463fe1b55d6f5d5ccfd
Robert Lodico <lodico.rj@gmail.com>
2020-05-03 22:03:53 -0400

    IFactory<T> and FactoryRegistry

---
+++ Intersect (Core)/Factories/IFactory.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace Intersect.Factories
+{
+    /// <summary>
+    /// Defines the interface for a factory class.
+    /// </summary>
+    /// <typeparam name="TValue">type of the created instances</typeparam>
+    public interface IFactory<out TValue>
+    {
+        /// <summary>
+        /// Creates a not-null instance of <typeparamref name="TValue"/> given the input <paramref name="args"/>.
+        /// </summary>
+        /// <param name="args">the optional arguments used in creating a <typeparamref name="TValue"/></param>
+        /// <returns>a not-null <typeparamref name="TValue"/></returns>
+        [NotNull]
+        TValue Create([NotNull] params object[] args);
+    }
+}
