04695885994df3638a9d1a95e78f473bb4caf00d
Robert Lodico <lodico.rj@gmail.com>
2020-07-25 16:02:05 -0400

    Core config/context/plugin/service interfaces

---
+++ Intersect (Core)/Plugins/IPluginBootstrapContext.cs
@@ -0,0 +1,18 @@
+﻿using Intersect.Plugins.Interfaces;
+
+using JetBrains.Annotations;
+
+namespace Intersect.Plugins
+{
+    /// <summary>
+    /// Defines the API of the plugin context during application bootstrapping.
+    /// </summary>
+    public interface IPluginBootstrapContext : IPluginBaseContext
+    {
+        /// <summary>
+        /// The <see cref="ICommandLineHelper"/> of the current plugin.
+        /// </summary>
+        [NotNull]
+        ICommandLineHelper CommandLine { get; }
+    }
+}
---
+++ Intersect (Core)/Plugins/IPluginEntry.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+
+namespace Intersect.Plugins
+{
+    /// <summary>
+    /// Defines the API that plugin entry points must implement
+    /// in order to be discovered by the plugin loader.
+    /// </summary>
+    /// <see cref="Loaders.PluginLoader.LoadFrom(Core.IApplicationContext, System.Reflection.Assembly)"/>
+    public interface IPluginEntry
+    {
+        /// <summary>
+        /// Invoked during application bootstrapping before startup.
+        /// </summary>
+        /// <param name="context">the current plugin context</param>
+        void OnBootstrap([NotNull] IPluginBootstrapContext context);
+
+        /// <summary>
+        /// Invoked during application startup after basic initialization.
+        /// </summary>
+        /// <param name="context">the current plugin context</param>
+        void OnStart([NotNull] IPluginContext context);
+
+        /// <summary>
+        /// Invoked during application shutdown.
+        /// </summary>
+        /// <param name="context">the current plugin context</param>
+        void OnStop([NotNull] IPluginContext context);
+    }
+}
