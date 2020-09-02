e304ca8dc6b67ba15dc2f6efd9ea8ca60f6278e3
Joyce <azurebeats@hotmail.nl>
2020-09-02 21:10:18 +0200

    Removed a derptastic test method.

--- Intersect.Client/MonoGame/File Management/MonoContentManager.cs
+++ Intersect.Client/MonoGame/File Management/MonoContentManager.cs
@@ -50,24 +50,6 @@ namespace Intersect.Client.MonoGame.File_Management
             TilesetsLoaded = true;
         }
 
-        public static void Decompress(FileInfo fileToDecompress)
-        {
-            using (FileStream originalFileStream = fileToDecompress.OpenRead())
-            {
-                string currentFileName = fileToDecompress.FullName;
-                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
-
-                using (FileStream decompressedFileStream = File.Create(newFileName))
-                {
-                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
-                    {
-                        decompressionStream.CopyTo(decompressedFileStream);
-                        Console.WriteLine("Decompressed: {0}", fileToDecompress.Name);
-                    }
-                }
-            }
-        }
-
         public override void LoadTexturePacks()
         {
             mTexturePackDict.Clear();
