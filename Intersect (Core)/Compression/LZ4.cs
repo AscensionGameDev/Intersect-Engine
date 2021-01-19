8596a2abcb85a68396350dd7b28dd04690cae215
JC Snider <jcsnider3@gmail.com>
2021-01-18 21:33:55 -0500

    Resolve double returns

--- Intersect (Core)/Compression/LZ4.cs
+++ Intersect (Core)/Compression/LZ4.cs
@@ -22,20 +22,12 @@ namespace Intersect.Compression
 
         public static byte[] PickleString(string data)
         {
-            return LZ4Pickler.Pickle(Encoding.Default.GetBytes(data), LZ4Level.L12_MAX);
-            var memoryStream = new MemoryStream();
-            var s = new StreamWriter(memoryStream);
-            s.Write(data);
-            return LZ4Pickler.Pickle(Encoding.Default.GetBytes(data), LZ4Level.L12_MAX);
+            return LZ4Pickler.Pickle(Encoding.Default.GetBytes(data), LZ4Level.L00_FAST);
         }
 
         public static string UnPickleString(byte[] data)
         {
             return Encoding.Default.GetString(LZ4Pickler.Unpickle(data));
-            data = LZ4Pickler.Unpickle(data);
-            var memoryStream = new MemoryStream(data);
-            var s = new StreamReader(memoryStream).ReadToEnd();
-            return s;
         }
     }
 }
