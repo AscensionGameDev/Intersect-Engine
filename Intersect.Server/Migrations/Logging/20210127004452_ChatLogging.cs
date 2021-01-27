a1e44845efb5510122075d021dc2d124450b94ba
marsh <marshmellowman0@hotmail.com>
2021-01-26 19:47:40 -0500

    Added migration properly

---
+++ Intersect.Server/Migrations/Logging/20210127004452_ChatLogging.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Intersect.Server.Migrations.Logging
+{
+    public partial class ChatLogging : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: true),
+                    PlayerId = table.Column<Guid>(nullable: true),
+                    PlayerName = table.Column<string>(nullable: true),
+                    Ip = table.Column<string>(nullable: true),
+                    TimeStamp = table.Column<DateTime>(nullable: false),
+                    MessageType = table.Column<int>(nullable: false),
+                    MessageText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatHistory", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatHistory");
+        }
+    }
+}
