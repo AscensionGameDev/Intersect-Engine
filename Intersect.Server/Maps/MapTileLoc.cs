cab7fa9c9169ce4e5c506d6fc1f1f07e019e25c1
JC Snider <jcsnider3@gmail.com>
2021-02-05 22:58:53 -0500

    Move MapTileLoc to its own file

---
+++ Intersect.Server/Maps/MapTileLoc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Intersect.Server.Maps
+{
+    public struct MapTileLoc
+    {
+        public readonly Guid MapId;
+        public readonly int X;
+        public readonly int Y;
+
+        public MapTileLoc(Guid id, int x, int y)
+        {
+            MapId = id;
+            X = x;
+            Y = y;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return MapId.GetHashCode() + (Y * Options.MapHeight) + X;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is MapTileLoc loc)
+            {
+                if (X == loc.X && Y == loc.Y && MapId == loc.MapId)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
