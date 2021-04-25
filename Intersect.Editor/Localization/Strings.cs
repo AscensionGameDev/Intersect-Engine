c6f106b8f45466b38560636511807b044b36c61f
JC Snider <jcsnider@users.noreply.github.com>
2021-04-24 21:17:50 -0400

    Added options to ignore cooldown reduction for items & spells. (#641)
    If ignore cdr is checked then cdr will not be applied for the group when that particular item/spell is used.

--- Intersect.Editor/Localization/Strings.cs
+++ Intersect.Editor/Localization/Strings.cs
@@ -2938,6 +2938,9 @@ Tick timer saved in server config.json.";
             [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
             public static LocalizedString IgnoreGlobalCooldown = @"Ignore Global Cooldown?";
 
+            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+            public static LocalizedString IgnoreCooldownReduction = @"Ignore Cooldown Reduction?";
+
             public static LocalizedString copy = @"Copy Item";
 
             public static LocalizedString critchance = @"Crit Chance (%):";
@@ -4190,6 +4193,9 @@ Tick timer saved in server config.json.";
 
             [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
             public static LocalizedString IgnoreGlobalCooldown = @"Ignore Global Cooldown?";
+
+            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+            public static LocalizedString IgnoreCooldownReduction = @"Ignore Cooldown Reduction?";
 
             public static LocalizedString copy = @"Copy Spell";
 
