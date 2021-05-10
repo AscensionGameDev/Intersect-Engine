9131d2cb07c6d361dc2b765b1a0628e98ba3cdc4
JC Snider <jcsnider@users.noreply.github.com>
2021-05-10 04:59:46 -0400

    feat: add utility methods and props to point/tex (#697)
    Co-authored-by: Robert Lodico <lodico.rj@gmail.com>

--- Intersect.Client.Framework/GenericClasses/Pointf.cs
+++ Intersect.Client.Framework/GenericClasses/Pointf.cs
@@ -5,6 +5,11 @@ namespace Intersect.Client.Framework.GenericClasses
 
     public struct Pointf
     {
+        public static Pointf Empty => new Pointf();
+
+        public static Pointf UnitX => new Pointf(1, 0);
+
+        public static Pointf UnitY => new Pointf(0, 1);
 
         private const float TOLERANCE = 0.001f;
 
@@ -20,9 +25,9 @@ namespace Intersect.Client.Framework.GenericClasses
 
         public override bool Equals(object obj)
         {
-            if (obj is Pointf)
+            if (obj is Pointf point)
             {
-                return (Pointf) obj == this;
+                return point == this;
             }
 
             return false;
@@ -33,12 +38,11 @@ namespace Intersect.Client.Framework.GenericClasses
             return Math.Abs(X - other.X) < TOLERANCE && Math.Abs(Y - other.Y) < TOLERANCE;
         }
 
-        public override int GetHashCode()
-        {
-            return X.GetHashCode() ^ Y.GetHashCode();
-        }
+        public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode();
 
-        public static Pointf Empty => new Pointf();
+        public Pointf StripX() => new Pointf(0, Y);
+
+        public Pointf StripY() => new Pointf(X, 0);
 
         public static bool operator !=(Pointf left, Pointf right)
         {
@@ -50,6 +54,13 @@ namespace Intersect.Client.Framework.GenericClasses
             return Math.Abs(left.X - right.X) < TOLERANCE && Math.Abs(left.Y - right.Y) < TOLERANCE;
         }
 
+        public static Pointf operator +(Pointf left, Pointf right) => new Pointf(left.X + right.X, left.Y + right.Y);
+
+        public static Pointf operator -(Pointf left, Pointf right) => new Pointf(left.X - right.X, left.Y - right.Y);
+
+        public static Pointf operator *(Pointf point, float scalar) => new Pointf(point.X * scalar, point.Y * scalar);
+
+        public static Pointf operator /(Pointf point, float scalar) => new Pointf(point.X / scalar, point.Y / scalar);
     }
 
 }
