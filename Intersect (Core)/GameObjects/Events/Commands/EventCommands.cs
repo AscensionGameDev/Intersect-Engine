bc69a70314d849fa16a1c74a11918d4174bbc1dc
AlexVild <ajv2324@gmail.com>
2021-06-29 14:38:00 -0400

    810: Remove unnecessary todo

--- Intersect (Core)/GameObjects/Events/Commands/EventCommands.cs
+++ Intersect (Core)/GameObjects/Events/Commands/EventCommands.cs
@@ -593,7 +593,7 @@ namespace Intersect.GameObjects.Events.Commands
 
         public WarpDirection Direction { get; set; } = WarpDirection.Retain;
 
-        public bool FadeOnWarp { get; set; } = false; // TODO Default to false; make configurable in editor config
+        public bool FadeOnWarp { get; set; } = false;
 
     }
 
