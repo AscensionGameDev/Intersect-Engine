65281bffc0d4a51481167ecf5e9466084b76f82b
Robert Lodico <lodico.rj@gmail.com>
2021-07-15 11:11:01 -0400

    chore: update nuget packages

--- scripts/package-framework.ps1
+++ scripts/package-framework.ps1
@@ -1,2 +1,15 @@
-nuget pack .\Intersect.Client.Framework\Intersect.Client.Framework.nuspec -OutputDirectory .\build\release\
-nuget pack .\Intersect.Server.Framework\Intersect.Server.Framework.nuspec -OutputDirectory .\build\release\
+$version="0.7.0.124-beta";
+for ($i = 0; $i -le $args.length; ++$i)
+{
+    if ("-Version".Equals($args[$i]))
+    {
+        if ($args.length -gt $i + 1)
+        {
+            $version = $args[++$i];
+        }
+    }
+}
+
+Write-Host "Building version " $version
+nuget pack .\Intersect.Client.Framework\Intersect.Client.Framework.nuspec -OutputDirectory .\build\release\ -Version $version
+nuget pack .\Intersect.Server.Framework\Intersect.Server.Framework.nuspec -OutputDirectory .\build\release\ -Version $version
