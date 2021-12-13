b6124add8fd51b7b8ab38d5688652b56af8a6f27
Cheshire <azurebeats@hotmail.nl>
2021-12-13 07:11:21 +0100

    Remove InviteRange from Parties (#963)

--- Intersect.Server/Networking/PacketHandler.cs
+++ Intersect.Server/Networking/PacketHandler.cs
@@ -1949,7 +1949,7 @@ namespace Intersect.Server.Networking
 
             var target = Player.FindOnline(packet.TargetId);
 
-            if (target != null && target.Id != player.Id && player.InRangeOf(target, Options.Party.InviteRange))
+            if (target != null && target.Id != player.Id)
             {
                 target.InviteToParty(player);
 
