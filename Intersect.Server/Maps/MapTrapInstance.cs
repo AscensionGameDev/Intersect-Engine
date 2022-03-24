3750d60109d837915ebd857438526aeeda5b4193
AlexVild <ajv2324@gmail.com>
2022-03-24 14:33:32 -0400

    fix: (Daywalkr) INSTANCING - Fix issue where traps would cease updating if their owner changed instances

--- Intersect.Server/Maps/MapTrapInstance.cs
+++ Intersect.Server/Maps/MapTrapInstance.cs
@@ -17,6 +17,8 @@ namespace Intersect.Server.Classes.Maps
         private long Duration;
 
         public Guid MapId;
+        
+        public Guid MapInstanceId;
 
         public Entity Owner;
 
@@ -30,12 +32,13 @@ namespace Intersect.Server.Classes.Maps
 
         public byte Z;
 
-        public MapTrapInstance(Entity owner, SpellBase parentSpell, Guid mapId, Guid mapLayerId, byte x, byte y, byte z)
+        public MapTrapInstance(Entity owner, SpellBase parentSpell, Guid mapId, Guid mapInstanceId, byte x, byte y, byte z)
         {
             Owner = owner;
             ParentSpell = parentSpell;
             Duration = Timing.Global.Milliseconds + ParentSpell.Combat.TrapDuration;
             MapId = mapId;
+            MapInstanceId = mapInstanceId;
             X = x;
             Y = y;
             Z = z;
@@ -72,7 +75,7 @@ namespace Intersect.Server.Classes.Maps
 
         public void Update()
         {
-            if (MapController.TryGetInstanceFromMap(MapId, Owner.MapInstanceId, out var mapInstance))
+            if (MapController.TryGetInstanceFromMap(MapId, MapInstanceId, out var mapInstance))
             {
                 if (Triggered)
                 {
