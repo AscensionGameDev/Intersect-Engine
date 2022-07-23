76abd8d82fc8e3c0f69582538c87ffe4ce4c88b7
Robbie Lodico <pandacoder@pm.me>
2022-07-23 02:03:46 -0400

    feat: implement boolean attribute

--- Intersect (Core)/GameObjects/Annotations/EditorBooleanAttribute.cs
+++ Intersect (Core)/GameObjects/Annotations/EditorBooleanAttribute.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 
 using Intersect.Localization;
 
@@ -8,5 +9,21 @@ namespace Intersect.GameObjects.Annotations
     public class EditorBooleanAttribute : EditorDisplayAttribute
     {
         public BooleanStyle Style { get; set; } = BooleanStyle.YesNo;
+
+        public override string Format(Type stringsType, object value)
+        {
+            if (stringsType == default)
+            {
+                throw new ArgumentNullException(nameof(stringsType));
+            }
+
+            var formatBooleanMethodInfo = stringsType.GetMethod("FormatBoolean", BindingFlags.Public | BindingFlags.Static);
+            if (formatBooleanMethodInfo == default)
+            {
+                throw new InvalidOperationException($"{stringsType.FullName}.{nameof(stringsType)} is missing.");
+            }
+
+            return formatBooleanMethodInfo.Invoke(null, new[] { value, Style })?.ToString();
+        }
     }
 }
