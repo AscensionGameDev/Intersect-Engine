593f3139aac232a0db625ff9c0dbb9fc80728e21
Robbie Lodico <pandacoder@pm.me>
2022-08-03 23:45:26 -0400

    feat: display network status - hack to ignore non-logged-in editors for testing - attempting to get descriptor loading

--- Intersect.Editor/Interface/Windows/DescriptorWindow.cs
+++ Intersect.Editor/Interface/Windows/DescriptorWindow.cs
@@ -2,6 +2,7 @@ using ImGuiNET;
 
 using Intersect.Client.Framework.UserInterface.Components;
 using Intersect.Editor.Localization;
+using Intersect.Editor.Networking;
 using Intersect.Enums;
 using Intersect.Time;
 
@@ -9,11 +10,30 @@ namespace Intersect.Editor.Interface.Windows;
 
 internal partial class DescriptorWindow : Window
 {
+    private readonly GameObjectType _descriptorType;
+
     public DescriptorWindow(GameObjectType descriptorType)
         : base(descriptorType.ToString())
     {
+        _descriptorType = descriptorType;
+
         Flags = ImGuiWindowFlags.AlwaysAutoResize;
-        Title = Strings.Windows.Descriptor.Title.ToString(Strings.Descriptors.Names[descriptorType].Singular);
+        Title = Strings.Windows.Descriptor.Title.ToString(Strings.Descriptors.Names[_descriptorType].Singular);
+
+        try
+        {
+            PacketSender.SendOpenEditor(_descriptorType);
+        }
+        catch
+        {
+            throw;
+        }
+    }
+
+    public bool HasPendingChanges
+    {
+        get => Flags.HasFlag(ImGuiWindowFlags.UnsavedDocument);
+        set => Flags = (Flags & ~ImGuiWindowFlags.UnsavedDocument) | (value ? ImGuiWindowFlags.UnsavedDocument : ImGuiWindowFlags.None);
     }
 
     protected override bool LayoutBegin(FrameTime frameTime)
@@ -23,9 +43,10 @@ internal partial class DescriptorWindow : Window
             return false;
         }
 
-        _ = ImGui.BeginChild(string.Empty, new(200, 100), true);
-
-        ImGui.EndChild();
+        if (!LayoutLookup(frameTime))
+        {
+            return false;
+        }
 
         return true;
     }
--- Intersect.Editor/UserInterface/Components/StatusBar.cs
+++ Intersect.Editor/UserInterface/Components/StatusBar.cs
@@ -2,6 +2,7 @@ using System.Numerics;
 
 using ImGuiNET;
 
+using Intersect.Network;
 using Intersect.Time;
 
 namespace Intersect.Client.Framework.UserInterface.Components;
@@ -18,9 +19,22 @@ public class StatusBar : Component
         | ImGuiWindowFlags.NoTitleBar
         ;
 
+    private bool _connectionDenied;
+    private NetworkStatus _networkStatus;
+
     public StatusBar(string name) : base(name)
     {
         Flags = StatusBarFlags;
+
+        Editor.Networking.Network.Socket.Connected += (_, connectionEventArgs) => _networkStatus = connectionEventArgs.NetworkStatus;
+
+        Editor.Networking.Network.Socket.ConnectionFailed += (_, connectionEventArgs, denied) =>
+        {
+            _connectionDenied = denied;
+            _networkStatus = connectionEventArgs.NetworkStatus;
+        };
+
+        Editor.Networking.Network.Socket.Disconnected += (_, connectionEventArgs) => _networkStatus = connectionEventArgs.NetworkStatus;
     }
 
     public ImGuiWindowFlags Flags { get; set; }
@@ -50,15 +64,7 @@ public class StatusBar : Component
         ImGui.PopStyleVar(2);
         ImGui.PopStyleColor();
 
-        ImGui.Text($"Viewport: {viewport.WorkSize}@{viewport.WorkPos} | Window Position: {windowPosition}");
-
-        ImGui.SameLine();
-
-        ImGui.Text("2Test status bar2");
-        //ImGui.Separator();
-
-        ImGui.SameLine();
-        ImGui.TextColored(new(1, 1, 0, 0), "test");
+        ImGui.Text($"Viewport: {viewport.WorkPos} {viewport.WorkSize} | Network Status: {_networkStatus} {(_connectionDenied ? "(Connection Denied)" : string.Empty)}");
 
         return true;
     }
