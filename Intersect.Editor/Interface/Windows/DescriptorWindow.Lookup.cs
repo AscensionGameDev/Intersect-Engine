890c2f2c5db5fe4c65093597b5bf630319fbf8eb
Robbie Lodico <pandacoder@pm.me>
2022-08-05 00:38:36 -0400

    feat: rudimentary tree line drawing

--- Intersect.Editor/Interface/Windows/DescriptorWindow.Lookup.cs
+++ Intersect.Editor/Interface/Windows/DescriptorWindow.Lookup.cs
@@ -1,9 +1,12 @@
+using System.Numerics;
+
 using ImGuiNET;
 
 using Intersect.Comparison;
 using Intersect.Editor.Localization;
 using Intersect.Enums;
 using Intersect.Models;
+using Intersect.Numerics;
 using Intersect.Time;
 
 namespace Intersect.Editor.Interface.Windows;
@@ -19,6 +22,69 @@ internal partial class DescriptorWindow
     private bool _groupByFolder = true;
     private string _searchQuery = string.Empty;
     private Guid _selectedObjectId;
+
+    private static void LayoutLookupGroup(ref Guid selectedObjectId, IGrouping<string?, IDatabaseObject> descriptorGroup)
+    {
+        var isNamedGroup = !string.IsNullOrEmpty(descriptorGroup.Key);
+        var drawList = ImGui.GetWindowDrawList();
+        var groupNodeRect = new FloatRect();
+        var treeIndent = ImGui.GetStyle().IndentSpacing;
+        var offsetVertical = 0f;
+        var treeLineColor = ImGui.GetColorU32(ImGuiCol.Text);
+
+        if (isNamedGroup)
+        {
+            var searchId = selectedObjectId;
+            var groupSelected = searchId != default && descriptorGroup.Any(descriptor => descriptor.Id == searchId);
+            var nodeFlags = groupSelected ? ImGuiTreeNodeFlags.Selected : ImGuiTreeNodeFlags.None;
+
+            // TODO: When the descriptor groups have true folders, descriptorGroup.Key will be replaced with a Guid
+            if (!ImGui.TreeNodeEx($"{descriptorGroup.Key?.Trim() ?? string.Empty}###descriptor_group_{descriptorGroup.Key}", nodeFlags))
+            {
+                return;
+            }
+
+            groupNodeRect = new(ImGui.GetItemRectMin(), ImGui.GetItemRectMax());
+            offsetVertical = ImGui.GetItemRectSize().Y;
+        }
+
+        var verticalStart = groupNodeRect.Position + Vector2.UnitX * treeIndent + Vector2.UnitY * offsetVertical / 2; //ImGui.GetCursorScreenPos() - Vector2.UnitY * 8;
+        verticalStart.X += 1 - treeIndent / 2;
+
+        var verticalEnd = verticalStart;
+
+        foreach (var descriptor in descriptorGroup)
+        {
+            ImGui.TreePush(descriptor.Id.ToString());
+
+            var currentlySelected = selectedObjectId == descriptor.Id;
+            if (ImGui.Selectable($"{descriptor.Name}###{descriptor.Id}", currentlySelected, ImGuiSelectableFlags.SpanAllColumns))
+            {
+                selectedObjectId = descriptor.Id;
+            }
+            else if (currentlySelected)
+            {
+                selectedObjectId = default;
+            }
+
+            var itemRect = new FloatRect(ImGui.GetItemRectMin(), ImGui.GetItemRectMax());
+            var midpoint = (ImGui.GetItemRectMax().Y + ImGui.GetItemRectMin().Y) / 2;
+            verticalEnd.Y = midpoint;
+
+            if (isNamedGroup)
+            {
+                drawList.AddLine(verticalEnd, verticalEnd + Vector2.UnitX * treeIndent, treeLineColor);
+            }
+
+            ImGui.TreePop();
+        }
+
+        if (isNamedGroup)
+        {
+            drawList.AddLine(verticalStart, verticalEnd, treeLineColor);
+            ImGui.TreePop();
+        }
+    }
 
     public virtual bool LayoutLookup(FrameTime frameTime)
     {
@@ -45,39 +111,7 @@ internal partial class DescriptorWindow
 
         foreach (var descriptorGroup in groupedDescriptors)
         {
-            if (!string.IsNullOrEmpty(descriptorGroup.Key))
-            {
-                var groupSelected = descriptorGroup.Any(descriptor => descriptor.Id == _selectedObjectId);
-                var nodeFlags = groupSelected ? ImGuiTreeNodeFlags.Selected : ImGuiTreeNodeFlags.None;
-
-                // TODO: When the descriptor groups have true folders, descriptorGroup.Key will be replaced with a Guid
-                if (!ImGui.TreeNodeEx($"{descriptorGroup.Key?.Trim() ?? string.Empty}###descriptor_group_{descriptorGroup.Key}", nodeFlags))
-                {
-                    continue;
-                }
-            }
-
-            foreach (var descriptor in descriptorGroup)
-            {
-                ImGui.TreePush(descriptor.Id.ToString());
-
-                var currentlySelected = _selectedObjectId == descriptor.Id;
-                if (ImGui.Selectable($"{descriptor.Name}###{descriptor.Id}", currentlySelected, ImGuiSelectableFlags.SpanAllColumns))
-                {
-                    _selectedObjectId = descriptor.Id;
-                }
-                else if (currentlySelected)
-                {
-                    _selectedObjectId = default;
-                }
-
-                ImGui.TreePop();
-            }
-
-            if (!string.IsNullOrEmpty(descriptorGroup.Key))
-            {
-                ImGui.TreePop();
-            }
+            LayoutLookupGroup(ref _selectedObjectId, descriptorGroup);
         }
 
         ImGui.EndChild();
