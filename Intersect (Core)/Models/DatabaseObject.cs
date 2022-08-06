72ccd8bcfea2951624e5f0cf3ca5dedeaa36be62
Robbie Lodico <pandacoder@pm.me>
2022-08-06 01:00:44 -0400

    chore: refactor non-lookup code into Descriptor

--- Intersect (Core)/Models/DatabaseObject.cs
+++ Intersect (Core)/Models/DatabaseObject.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
 using Intersect.Collections;
 using Intersect.Enums;
 
@@ -7,84 +5,22 @@ using Newtonsoft.Json;
 
 namespace Intersect.Models;
 
-public abstract partial class DatabaseObject<TObject> : IDatabaseObject where TObject : DatabaseObject<TObject>
+public abstract partial class DatabaseObject<TObject>
+    : Descriptor, IDatabaseObject
+    where TObject : DatabaseObject<TObject>
 {
-
     public const string Deleted = "ERR_DELETED";
 
-    private string mBackup;
-
-    protected DatabaseObject() : this(Guid.Empty) { }
+    protected DatabaseObject() : base() { }
 
     [JsonConstructor]
-    protected DatabaseObject(Guid guid)
-    {
-        Id = guid;
-        TimeCreated = DateTime.Now.ToBinary();
-    }
+    protected DatabaseObject(Guid guid) : base(guid) { }
 
-    public static KeyValuePair<Guid, string>[] ItemPairs => Lookup.OrderBy(p => p.Value?.Name)
-        .Select(pair => new KeyValuePair<Guid, string>(pair.Key, pair.Value?.Name ?? Deleted))
-        .ToArray();
+    public override GameObjectType Type => LookupUtils.GetGameObjectType(typeof(TObject));
 
-    public static string[] Names => Lookup.OrderBy(p => p.Value?.Name)
-        .Select(pair => pair.Value?.Name ?? Deleted)
-        .ToArray();
+    public override void Delete() => Lookup.Delete(this as TObject);
 
-    public static DatabaseObjectLookup Lookup => LookupUtils.GetLookup(typeof(TObject));
-
-    [DatabaseGenerated(DatabaseGeneratedOption.None)]
-    public Guid Id { get; protected set; } = Guid.NewGuid();
-
-    public long TimeCreated { get; set; }
-
-    [JsonIgnore]
-    [NotMapped]
-    public GameObjectType Type => LookupUtils.GetGameObjectType(typeof(TObject));
-
-    [JsonIgnore]
-    [NotMapped]
-    public string DatabaseTable => Type.GetTable();
-
-    [JsonProperty(Order = -4)]
-    [Column(Order = 0)]
-    public string Name { get; set; }
-
-    public virtual void Load(string json, bool keepCreationTime = false)
-    {
-        var oldTime = TimeCreated;
-        JsonConvert.PopulateObject(
-            json, this, new JsonSerializerSettings
-            {
-                ObjectCreationHandling = ObjectCreationHandling.Replace
-            }
-        );
-
-        if (keepCreationTime)
-        {
-            TimeCreated = oldTime;
-        }
-    }
-
-    public void MakeBackup() => mBackup = JsonData;
-
-    public void DeleteBackup() => mBackup = null;
-
-    public void RestoreBackup()
-    {
-        if (mBackup != null)
-        {
-            Load(mBackup);
-        }
-    }
-
-    [JsonIgnore]
-    [NotMapped]
-
-    // TODO: Should eventually be formatting.none
-    public virtual string JsonData => JsonConvert.SerializeObject(this, Formatting.Indented);
-
-    public virtual void Delete() => Lookup.Delete(this as TObject);
+    public int ListIndex() => ListIndex(Id);
 
     public static Guid IdFromList(int listIndex)
     {
@@ -107,7 +43,15 @@ public abstract partial class DatabaseObject<TObject> : IDatabaseObject where TO
             listIndex];
     }
 
-    public int ListIndex() => ListIndex(Id);
+    public static KeyValuePair<Guid, string>[] ItemPairs => Lookup.OrderBy(p => p.Value?.Name)
+        .Select(pair => new KeyValuePair<Guid, string>(pair.Key, pair.Value?.Name ?? Deleted))
+        .ToArray();
+
+    public static string[] Names => Lookup.OrderBy(p => p.Value?.Name)
+        .Select(pair => pair.Value?.Name ?? Deleted)
+        .ToArray();
+
+    public static DatabaseObjectLookup Lookup => LookupUtils.GetLookup(typeof(TObject));
 
     public static int ListIndex(Guid id) =>
         Lookup.KeyList.OrderBy(pairs => Lookup[pairs]?.Name).ToList().IndexOf(id);
