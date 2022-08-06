661b83fa1024958c93fd2bcce48e02198b53b650
Robbie Lodico <pandacoder@pm.me>
2022-08-06 02:56:53 -0400

    feat: generate migration AddContentString

--- Intersect.Server/Migrations/Game/20220806063743_AddContentString.Designer.cs
+++ Intersect.Server/Migrations/Game/20220806063743_AddContentString.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Intersect.Server.Database.GameData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Intersect.Server.Migrations.Game
 {
     [DbContext(typeof(GameContext))]
-    partial class GameContextModelSnapshot : ModelSnapshot
+    [Migration("20220806063743_AddContentString")]
+    partial class AddContentString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.3");
