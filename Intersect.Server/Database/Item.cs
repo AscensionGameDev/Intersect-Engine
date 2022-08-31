0917da9b7da4fb08c6c59251d2bb62bcfd934982
JC Snider <jcsnider@users.noreply.github.com>
2022-08-31 13:52:08 -0400

    fix bags with deleted items have slots taken by ghost items (#1336)
    * Correct any bank slots on load by removing items which have been deleted
    
    * Cleaner code.
    
    * chore: rearrange if statements
    
    Co-authored-by: Robbie Lodico <pandacoder@pm.me>

--- Intersect.Server/Database/Item.cs
+++ Intersect.Server/Database/Item.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
 using Intersect.Enums;
@@ -122,16 +122,25 @@ namespace Intersect.Server.Database
         {
             bag = Bag;
 
-            // ReSharper disable once InvertIf Justification: Do not introduce two different return points that assert a value state
             if (bag == null)
             {
                 var descriptor = Descriptor;
-                // ReSharper disable once InvertIf Justification: Do not introduce two different return points that assert a value state
                 if (descriptor?.ItemType == ItemTypes.Bag)
                 {
                     bag = Bag.GetBag(BagId ?? Guid.Empty);
                     bag?.ValidateSlots();
                     Bag = bag;
+                }
+            }
+            else
+            {
+                // Remove any items from this bag that have been removed from the game
+                foreach (var slot in bag.Slots)
+                {
+                    if (ItemBase.Get(slot.ItemId) == default)
+                    {
+                        slot.Set(None);
+                    }
                 }
             }
 
