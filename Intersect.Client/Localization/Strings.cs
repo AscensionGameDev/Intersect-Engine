3653d12cd46efd593de93842ceee84bc0f57d2b2
WeylonSantana <63019821+WeylonSantana@users.noreply.github.com>
2022-08-31 16:27:40 -0300

    Extra Buffs in Character Window (#1417)
    * Extra Buffs in Character Window
    
    * Panda Review

--- Intersect.Client/Localization/Strings.cs
+++ Intersect.Client/Localization/Strings.cs
@@ -386,6 +386,24 @@ namespace Intersect.Client.Localization
 
             public static LocalizedString title = @"Character";
 
+            public static LocalizedString ExtraBuffs = @"Extra Buffs";
+
+            public static LocalizedString HealthRegen = @"Health Regen: {00}%";
+
+            public static LocalizedString ManaRegen = @"Mana Regen: {00}%";
+
+            public static LocalizedString Lifesteal = @"Lifesteal: {00}%";
+
+            public static LocalizedString AttackSpeed = @"Attack Speed: {00}s";
+
+            public static LocalizedString ExtraExp = @"Bonus EXP: {00}%";
+
+            public static LocalizedString Luck = @"Luck: {00}%";
+
+            public static LocalizedString Tenacity = @"Tenacity: {00}%";
+
+            public static LocalizedString CooldownReduction = @"Cooldown Reduction: {00}%";
+
         }
 
         public partial struct CharacterCreation
