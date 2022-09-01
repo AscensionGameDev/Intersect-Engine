236a63b778512d31287c028d5c752a5a0cbc3d9d
Robbie Lodico <pandacoder@pm.me>
2022-09-01 19:15:49 -0400

    fix: hostbuilder for migrations

--- Intersect.Server/Core/Program.cs
+++ Intersect.Server/Core/Program.cs
@@ -3,11 +3,16 @@ using System.Diagnostics;
 using System.Globalization;
 using System.Reflection;
 using Intersect.Config;
+using Intersect.Server.Database;
 using Intersect.Server.Database.GameData;
+using Intersect.Server.Database.Logging;
+using Intersect.Server.Database.PlayerData;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySqlConnector;
 
 namespace Intersect.Server.Core
 {
@@ -17,7 +22,6 @@ namespace Intersect.Server.Core
     /// </summary>
     public static partial class Program
     {
-
         [STAThread]
         public static void Main(string[] args)
         {
@@ -42,8 +46,34 @@ namespace Intersect.Server.Core
             }
         }
 
+        /// <summary>
+        /// Host builder method for Entity Framework Design Tools to use when generating migrations.
+        /// </summary>
         public static IHostBuilder CreateHostBuilder(string[] args) => Host
             .CreateDefaultBuilder(args)
-            .ConfigureServices((hostContext, services) => { });
+            .ConfigureServices((hostContext, services) =>
+            {
+                var rawDatabaseType = hostContext.Configuration.GetValue<string>("DatabaseType") ??
+                                      DatabaseType.Sqlite.ToString();
+                if (!Enum.TryParse(rawDatabaseType, out DatabaseType databaseType))
+                {
+                    throw new InvalidOperationException($"Invalid database type: {rawDatabaseType}");
+                }
+
+                var connectionString = hostContext.Configuration.GetValue<string>("ConnectionString");
+                DbConnectionStringBuilder connectionStringBuilder = databaseType switch
+                {
+                    DatabaseType.MySql => new MySqlConnectionStringBuilder(connectionString),
+                    DatabaseType.Sqlite => new SqliteConnectionStringBuilder(connectionString),
+                    _ => throw new IndexOutOfRangeException($"Unsupported database type: {databaseType}")
+                };
+
+                DatabaseContextOptions databaseContextOptions = new()
+                {
+                    ConnectionStringBuilder = connectionStringBuilder, DatabaseType = databaseType
+                };
+
+                services.AddSingleton(databaseContextOptions);
+            });
     }
 }
