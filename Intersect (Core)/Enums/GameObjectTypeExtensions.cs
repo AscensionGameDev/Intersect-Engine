736822072bb419d1d353c3be86068f49fbf650f6
Robbie Lodico <pandacoder@pm.me>
2022-09-03 20:40:25 -0400

    chore: cleanup GameObjectTypeExtensions, add GetLocalizedName() - organize methods - cleanup code - add GetLocalizedName()

--- Intersect (Core)/Enums/GameObjectTypeExtensions.cs
+++ Intersect (Core)/Enums/GameObjectTypeExtensions.cs
@@ -1,59 +1,60 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
 using Intersect.Collections;
 using Intersect.Extensions;
+using Intersect.Localization.Common.Descriptors;
 using Intersect.Models;
 
-namespace Intersect.Enums
+namespace Intersect.Enums;
+
+public static partial class GameObjectTypeExtensions
 {
-
-    public static partial class GameObjectTypeExtensions
+    static GameObjectTypeExtensions()
     {
+        EnumType = typeof(GameObjectType);
+        AttributeType = typeof(GameObjectInfoAttribute);
+        AttributeMap = new();
 
-        static GameObjectTypeExtensions()
+        foreach (GameObjectType gameObjectType in Enum.GetValues(EnumType))
         {
-            EnumType = typeof(GameObjectType);
-            AttributeType = typeof(GameObjectInfoAttribute);
-            AttributeMap = new Dictionary<GameObjectType, GameObjectInfoAttribute>();
-
-            foreach (GameObjectType gameObjectType in Enum.GetValues(EnumType))
-            {
-                var memberInfo = EnumType.GetMember(Enum.GetName(EnumType, value: gameObjectType)).FirstOrDefault();
-                AttributeMap[gameObjectType] =
-                    (GameObjectInfoAttribute) memberInfo?.GetCustomAttributes(AttributeType, false).FirstOrDefault();
-            }
+            var memberInfo = EnumType.GetMember(Enum.GetName(EnumType, value: gameObjectType)).FirstOrDefault();
+            AttributeMap[gameObjectType] =
+                (GameObjectInfoAttribute)memberInfo?.GetCustomAttributes(AttributeType, false).FirstOrDefault();
         }
-
-        private static Type EnumType { get; }
-
-        private static Type AttributeType { get; }
-
-        private static Dictionary<GameObjectType, GameObjectInfoAttribute> AttributeMap { get; }
-
-        public static Type GetObjectType(this GameObjectType gameObjectType)
-        {
-            return AttributeMap?[gameObjectType]?.Type;
-        }
-
-        public static string GetTable(this GameObjectType gameObjectType)
-        {
-            return AttributeMap?[gameObjectType]?.Table;
-        }
-
-        public static DatabaseObjectLookup GetLookup(this GameObjectType gameObjectType)
-        {
-            return LookupUtils.GetLookup(GetObjectType(gameObjectType));
-        }
-
-        public static IDatabaseObject CreateNew(this GameObjectType gameObjectType)
-        {
-            var instance = Activator.CreateInstance(AttributeMap?[gameObjectType]?.Type, new object[] { });
-
-            return instance as IDatabaseObject;
-        }
-
     }
 
+    private static Type EnumType { get; }
+
+    private static Type AttributeType { get; }
+
+    private static Dictionary<GameObjectType, GameObjectInfoAttribute> AttributeMap { get; }
+
+    public static IDatabaseObject CreateNew(this GameObjectType gameObjectType)
+    {
+        var instance = Activator.CreateInstance(AttributeMap?[gameObjectType]?.Type, new object[] { });
+
+        return instance as IDatabaseObject;
+    }
+
+    public static Type GetObjectType(this GameObjectType gameObjectType)
+    {
+        return AttributeMap?[gameObjectType]?.Type;
+    }
+
+    public static DatabaseObjectLookup GetLookup(this GameObjectType gameObjectType)
+    {
+        return LookupUtils.GetLookup(GetObjectType(gameObjectType));
+    }
+
+    public static string GetLocalizedName(
+        this GameObjectType gameObjectType,
+        DescriptorsNamespace descriptorsNamespace,
+        int count = 1
+    ) => descriptorsNamespace.Names[gameObjectType].ToString(count);
+
+    public static string GetTable(this GameObjectType gameObjectType)
+    {
+        return AttributeMap?[gameObjectType]?.Table;
+    }
 }
