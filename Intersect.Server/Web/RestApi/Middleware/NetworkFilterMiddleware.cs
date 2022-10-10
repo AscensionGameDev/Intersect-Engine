c65a96f4459f0edf5320fa2c4815f86fc86aa5d7
Robbie Lodico <pandacoder@pm.me>
2022-10-10 14:58:24 -0400

    fix: harden network security

---
+++ Intersect.Server/Web/RestApi/Middleware/NetworkFilterMiddleware.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+using Intersect.Server.Web.RestApi.Configuration;
+using Intersect.Server.Web.RestApi.Types;
+
+using Microsoft.Owin;
+
+namespace Intersect.Server.Web.RestApi.Middleware
+{
+    internal class NetworkFilterMiddleware : OwinMiddleware
+    {
+        private const string SCHEME_HTTPS = "https";
+
+        private readonly NetworkTypes _allowedNetworkTypes;
+
+        public NetworkFilterMiddleware(OwinMiddleware next, NetworkTypes allowedNetworkTypes) : base(next)
+        {
+            _allowedNetworkTypes = allowedNetworkTypes;
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            if (context == default)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var request = context.Request;
+
+            if (SCHEME_HTTPS.Equals(request.Scheme, StringComparison.OrdinalIgnoreCase))
+            {
+                return Next?.Invoke(context);
+            }
+
+            if (!IPAddress.TryParse(request.RemoteIpAddress, out var remoteIpAddress))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                return Task.CompletedTask;
+            }
+
+            foreach (var networkTypeFlag in NetworkTypeFlags)
+            {
+                if (_allowedNetworkTypes.HasFlag(networkTypeFlag))
+                {
+                    var ipRanges = IpRangesForNetworkType[networkTypeFlag];
+                    if (ipRanges.Any(range => range.IsInRange(remoteIpAddress)))
+                    {
+                        return Next?.Invoke(context);
+                    }
+                }
+            }
+
+            context.Response.StatusCode = (int)HttpStatusCode.HttpVersionNotSupported;
+            return Task.CompletedTask;
+        }
+
+        private static readonly IDictionary<NetworkTypes, IpRange[]> IpRangesForNetworkType = new Dictionary<NetworkTypes, IpRange[]>
+        {
+            {
+                NetworkTypes.Loopback,
+                new[]
+                {
+                    new IpRange(IPAddress.Parse("0.0.0.0"), IPAddress.Parse("0.255.255.255")),
+                    new IpRange(IPAddress.Parse("127.0.0.0"), IPAddress.Parse("127.255.255.255"))
+                }
+            },
+            {
+                NetworkTypes.Subnet,
+                new[]
+                {
+                    new IpRange(IPAddress.Parse("169.254.0.0"), IPAddress.Parse("169.254.255.255")),
+                    new IpRange(IPAddress.Parse("255.255.255.255")),
+                }
+            },
+            {
+                NetworkTypes.PrivateNetwork,
+                new[]
+                {
+                    new IpRange(IPAddress.Parse("10.0.0.0"), IPAddress.Parse("10.255.255.255")),
+                    new IpRange(IPAddress.Parse("100.64.0.0"), IPAddress.Parse("100.127.255.255")),
+                    new IpRange(IPAddress.Parse("172.16.0.0"), IPAddress.Parse("172.31.255.255")),
+                    new IpRange(IPAddress.Parse("192.0.0.0"), IPAddress.Parse("192.0.0.255")),
+                    new IpRange(IPAddress.Parse("192.168.0.0"), IPAddress.Parse("192.168.255.255")),
+                    new IpRange(IPAddress.Parse("192.18.0.0"), IPAddress.Parse("192.19.255.255")),
+                }
+            }
+        };
+
+        private static readonly NetworkTypes[] NetworkTypeFlags = Enum.GetValues(typeof(NetworkTypes)).OfType<NetworkTypes>().ToArray();
+    }
+}
---
+++ Intersect.Server/Web/RestApi/Types/IpRange.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Net;
+
+namespace Intersect.Server.Web.RestApi.Types
+{
+    internal class IpRange
+    {
+        public IpRange(IPAddress address)
+        {
+            if (address == null)
+            {
+                throw new ArgumentNullException(nameof(address));
+            }
+
+            Start = address.GetAddressBytes();
+            End = address.GetAddressBytes();
+        }
+
+        public IpRange(IPAddress start, IPAddress end)
+        {
+            if (start == default)
+            {
+                throw new ArgumentNullException(nameof(start));
+            }
+
+            if (end == default)
+            {
+                throw new ArgumentNullException(nameof(end));
+            }
+
+            if (start.AddressFamily != end.AddressFamily)
+            {
+                throw new ArgumentException("AddressFamily mismatch");
+            }
+
+            Start = start.GetAddressBytes();
+            End = end.GetAddressBytes();
+        }
+
+        public byte[] Start { get; }
+
+        public byte[] End { get; }
+
+        public bool IsInRange(IPAddress address)
+        {
+            if (Start == default || End == default || address == default) {
+                return false;
+            }
+
+            var octets = address.GetAddressBytes();
+            if (Start.Length != End.Length || End.Length != octets.Length)
+            {
+                return false;
+            }
+
+            for (var index = 0; index < octets.Length; index++)
+            {
+                var start = Start[index];
+                var end = End[index];
+                var octet = octets[index];
+
+                if (octet < start || end < octet)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
