073b786be7bf143aca59173ff43ef8e867e78162
Robbie Lodico <pandacoder@pm.me>
2022-10-16 13:40:37 -0400

    chore: generate migrations

---
+++ Intersect.Server/Migrations/Game/20221016173617_AddUserVariables.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Intersect.Server.Migrations.Game
+{
+    public partial class AddUserVariables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserVariables",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TimeCreated = table.Column<long>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    TextId = table.Column<string>(nullable: true),
+                    DataType = table.Column<byte>(nullable: false),
+                    Folder = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserVariables", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserVariables");
+        }
+    }
+}
