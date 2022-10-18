24a7c286261ab51488e1794fe9cb58fc16f81cb5
lodicolo <pandacoder@pm.me>
2022-10-18 18:33:06 -0400

    fix: prevent future dataloss, properly recreate the tables (#1634)

--- Intersect.Server/Migrations/Game/20221017220000_DropColumns.cs
+++ Intersect.Server/Migrations/Game/20221017220000_DropColumns.cs
@@ -8,14 +8,7 @@ namespace Intersect.Server.Migrations.Game
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             if (migrationBuilder.ActiveProvider == "Microsoft.EntityFrameworkCore.Sqlite") {
-                _ = migrationBuilder.Sql(
-                    @"
-                    CREATE TABLE Items__migration AS SELECT Id, TimeCreated, Name, Animation, AttackAnimation, EquipmentAnimation, Bound, CritChance, CritMultiplier, Cooldown, Damage, DamageType, AttackSpeedModifier, AttackSpeedValue, Consumable_Type, Consumable_Value, EquipmentSlot, TwoHanded, Effects, SlotCount, Spell, Event, Description, FemalePaperdoll, ItemType, MalePaperdoll, Icon, Price, Projectile, Scaling, ScalingStat, Speed, Stackable, StatGrowth, Tool, VitalsGiven, StatsGiven, UsageRequirements, DestroySpell, QuickCast, Rarity, Folder, Consumable_Percentage, PercentageStatsGiven, PercentageVitalsGiven, VitalsRegen, CooldownGroup, IgnoreGlobalCooldown, Color, IgnoreCooldownReduction, CanBag, CanBank, DropChanceOnDeath, CanSell, CanTrade, MaxBankStack, MaxInventoryStack, CanGuildBank, CannotUseMessage, BlockAbsorption, BlockAmount, BlockChance, WeaponSpriteOverride, DespawnTime FROM Items;
-                    DROP TABLE Items;
-                    CREATE TABLE Items AS SELECT * FROM Items__migration;
-                    DROP TABLE Items__migration;
-                    "
-                );
+                // Removed because this will wipe out anything fkey reliant
             }
         }
 
