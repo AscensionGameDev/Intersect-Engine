3a415f19611c34e4db7aaefe0e57ec60ea84ed83
Robbie Lodico <pandacoder@pm.me>
2023-02-08 10:51:22 -0500

    fix: change 505 to 426 upgrade to tls

--- Intersect.Server/Web/RestApi/Middleware/NetworkFilterMiddleware.cs
+++ Intersect.Server/Web/RestApi/Middleware/NetworkFilterMiddleware.cs
@@ -54,7 +54,11 @@ namespace Intersect.Server.Web.RestApi.Middleware
                 }
             }
 
-            context.Response.StatusCode = (int)HttpStatusCode.HttpVersionNotSupported;
+            context.Response.StatusCode = (int)HttpStatusCode.UpgradeRequired;
+            context.Response.ReasonPhrase = "HTTPS Required";
+            context.Response.Headers["Upgrade"] = "TLS/1.2";
+            context.Response.Headers["Upgrade-Insecure-Requests"] = "1";
+            context.Response.Body.Close();
             return Task.CompletedTask;
         }
 
