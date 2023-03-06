f29c43d29d5c80284ef15af12b7d6cc1ae51ca45
AlexVild <ajv2324@gmail.com>
2023-03-06 11:25:49 -0500

    chore: (Day) Update logic in settings window to respect enum type for typewriter setting

--- Intersect.Client/Interface/Shared/SettingsWindow.cs
+++ Intersect.Client/Interface/Shared/SettingsWindow.cs
@@ -689,7 +689,7 @@ namespace Intersect.Client.Interface.Shared
             mPlayerOverheadHpBarCheckbox.IsChecked = Globals.Database.PlayerOverheadHpBar;
             mStickyTarget.IsChecked = Globals.Database.StickyTarget;
             mAutoTurnToTarget.IsChecked = Globals.Database.AutoTurnToTarget;
-            mTypewriterCheckbox.IsChecked = Globals.Database.TypewriterText;
+            mTypewriterCheckbox.IsChecked = Globals.Database.TypewriterText == Enums.TypewriterTypes.Word;
 
             // Video Settings.
             mFullscreenCheckbox.IsChecked = Globals.Database.FullScreen;
@@ -869,7 +869,7 @@ namespace Intersect.Client.Interface.Shared
             Globals.Database.PlayerOverheadHpBar = mPlayerOverheadHpBarCheckbox.IsChecked;
             Globals.Database.StickyTarget = mStickyTarget.IsChecked;
             Globals.Database.AutoTurnToTarget = mAutoTurnToTarget.IsChecked;
-            Globals.Database.TypewriterText = mTypewriterCheckbox.IsChecked;
+            Globals.Database.TypewriterText = mTypewriterCheckbox.IsChecked ? Enums.TypewriterTypes.Word : Enums.TypewriterTypes.Off;
 
             // Video Settings.
             var resolution = mResolutionList.SelectedItem;
