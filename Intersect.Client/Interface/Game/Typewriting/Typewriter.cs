0b177ed5bbb32254002a56ed21a52e1e6629d690
Arufonsu <17498701+Arufonsu@users.noreply.github.com>
2023-03-07 16:14:46 -0300

    chore and fix: typewriter
    - fixes an issue where the typewriter would stop working after typing it's first dialogue.
    - refactors typewriter and it's write logic bit.

--- Intersect.Client/Interface/Game/Typewriting/Typewriter.cs
+++ Intersect.Client/Interface/Game/Typewriting/Typewriter.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Intersect.Client.Core;
 using Intersect.Client.Framework.Gwen.Control;
 using Intersect.Configuration;
@@ -10,50 +7,50 @@ using Intersect.Utilities;
 
 namespace Intersect.Client.Interface.Game.Typewriting
 {
-
     internal sealed class Typewriter
     {
-        private static List<char> _fullstopChars => ClientConfiguration.Instance.TypewriterFullStopCharacters;
+        private static List<char> _fullStopChars => ClientConfiguration.Instance.TypewriterFullStopCharacters;
         private static long _fullStopSpeed => ClientConfiguration.Instance.TypewriterFullStopDelay;
-        private static List<char> _partialstopChars => ClientConfiguration.Instance.TypewriterPauseCharacters;
+        private static List<char> _partialStopChars => ClientConfiguration.Instance.TypewriterPauseCharacters;
         private static long _partialStopSpeed => ClientConfiguration.Instance.TypewriterPauseDelay;
         private static long _typingSpeed => ClientConfiguration.Instance.TypewriterPartDelay;
 
         private List<Label> _labels;
         private string[] _lines;
-        private Label _currentLabel => _labels.ElementAtOrDefault(_lineIndex);
-        private string _currentLine => _lines.ElementAtOrDefault(_lineIndex);
+        private int _lineCount;
         private int _lineIndex;
         private int _charIndex;
         private char? _lastChar;
         private long _nextUpdateTime;
 
         public bool IsDone { get; private set; }
-
         public long DoneAtMilliseconds { get; private set; }
-
-        public void NewLine()
-        {
-            _lineIndex++;
-            if (_lineIndex >= _lines.Length)
-            {
-                End();
-                return;
-            }
-            _charIndex = 0;
-            _lastChar = null;
-        }
 
         public void Initialize(List<Label> labels)
         {
             _nextUpdateTime = Timing.Global.Milliseconds;
             _labels = labels;
             _lines = _labels.Select(l => l.Text).ToArray();
-            _labels.ForEach(l => l.SetText(string.Empty)); // Clear the text from the labels. The writer is the captain now
+            _labels.ForEach(l =>
+                l.SetText(string.Empty)); // Clear the text from the labels. The writer is the captain now
             _lineIndex = 0;
             _charIndex = 0;
             _lastChar = null;
+            _lineCount = _lines.Length;
             IsDone = false;
+        }
+
+        private void NewLine()
+        {
+            _lineIndex++;
+            if (_lineIndex >= _lineCount)
+            {
+                End();
+                return;
+            }
+
+            _charIndex = 0;
+            _lastChar = null;
         }
 
         public void Write(string voice)
@@ -63,7 +60,7 @@ namespace Intersect.Client.Interface.Game.Typewriting
                 return;
             }
 
-            if (string.IsNullOrWhiteSpace(_currentLine) || _currentLabel == default)
+            if (_lineIndex >= _lineCount || _labels[_lineIndex] == null)
             {
                 End();
                 return;
@@ -80,20 +77,21 @@ namespace Intersect.Client.Interface.Game.Typewriting
                 Audio.AddGameSound(voice ?? string.Empty, false);
             }
 
+            var currentLine = _lines[_lineIndex];
             _charIndex++;
-            if (_charIndex >= _currentLine.Length)
+
+            if (_charIndex >= currentLine.Length)
             {
-                _currentLabel?.SetText(_currentLine);
+                _labels[_lineIndex].SetText(currentLine);
                 NewLine();
                 return;
             }
 
-            _lastChar = _currentLine[_charIndex - 1];
+            _lastChar = currentLine[_charIndex - 1];
+            var written = currentLine.Substring(0, _charIndex);
+            _labels[_lineIndex].SetText(written);
 
-            var written = _currentLine?.Substring(0, _charIndex) ?? string.Empty;
-            _currentLabel?.SetText(written);
-
-            _nextUpdateTime = Timing.Global.Milliseconds + GetTypingDelayFor(_currentLine.ElementAtOrDefault(_charIndex), _lastChar.Value);
+            _nextUpdateTime = Timing.Global.Milliseconds + GetTypingDelayFor(currentLine[_charIndex], _lastChar);
         }
 
         private static long GetTypingDelayFor(char currentChar, char? lastChar)
@@ -103,17 +101,19 @@ namespace Intersect.Client.Interface.Game.Typewriting
                 return _typingSpeed;
             }
 
-            var lastCharVal = lastChar.GetValueOrDefault();
-            // Allows things like ellipses
+            var lastCharVal = lastChar.Value;
+
             if (currentChar == lastCharVal)
             {
                 return _typingSpeed;
             }
-            if (_fullstopChars.Contains(lastCharVal))
+
+            if (_fullStopChars.Contains(lastCharVal))
             {
                 return _fullStopSpeed;
             }
-            if (_partialstopChars.Contains(lastCharVal))
+
+            if (_partialStopChars.Contains(lastCharVal))
             {
                 return _partialStopSpeed;
             }
@@ -128,7 +128,7 @@ namespace Intersect.Client.Interface.Game.Typewriting
                 return;
             }
 
-            for (var i = 0; i < _lines.Length; i++)
+            for (var i = 0; i < _lineCount; i++)
             {
                 if (i >= _labels.Count)
                 {
