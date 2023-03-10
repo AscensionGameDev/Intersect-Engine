c71c554efda63544047d089d4c68f29b3a132b72
Robbie Lodico <pandacoder@pm.me>
2023-03-09 23:45:42 -0500

    fix: comment swagger completely out until we can resolve the W10 crash

--- Intersect.Server/Web/RestApi/Swagger/AuthorizationFilter.cs
+++ Intersect.Server/Web/RestApi/Swagger/AuthorizationFilter.cs
@@ -1,26 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Web.Http;
-using System.Web.Http.Description;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Net;
+// using System.Web.Http;
+// using System.Web.Http.Description;
 
-using Swashbuckle.Swagger;
+// using Swashbuckle.Swagger;
 
-namespace Intersect.Server.Web.RestApi.Swagger
-{
-    internal class AuthorizationFilter : IOperationFilter
-    {
-        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
-        {
-            var authorizeAttributes = apiDescription.GetControllerAndActionAttributes<AuthorizeAttribute>();
-            if (authorizeAttributes.Any())
-            {
-                operation.responses.Add(
-                    $"{(int)HttpStatusCode.Unauthorized}",
-                    new Response { description = "Unauthorized" }
-                );
-            }
-        }
-    }
-}
+// namespace Intersect.Server.Web.RestApi.Swagger
+// {
+//     internal class AuthorizationFilter : IOperationFilter
+//     {
+//         public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+//         {
+//             var authorizeAttributes = apiDescription.GetControllerAndActionAttributes<AuthorizeAttribute>();
+//             if (authorizeAttributes.Any())
+//             {
+//                 operation.responses.Add(
+//                     $"{(int)HttpStatusCode.Unauthorized}",
+//                     new Response { description = "Unauthorized" }
+//                 );
+//             }
+//         }
+//     }
+// }
