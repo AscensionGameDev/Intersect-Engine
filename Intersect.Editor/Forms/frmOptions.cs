9d832c909fa8f39ac830df6ce6909be8a7424164
Arufonsu <17498701+Arufonsu@users.noreply.github.com>
2023-08-27 14:55:57 -0400

    feat: map editor cursor sprites (#1797)
    * feat: map editor cursor sprites
    
    * Feature can be toggled within the editor -> File -> Options -> General
     - Cursor sprite changes when the user switches between tools.
     - Moving the cursor out of the map editor sets the cursor back to normal.
     - Editor tool's custom sprites are modifiable.
     - Editor tool's cursor click-points are configurable.
    
    * Fixed issues when switching from selection tool to other tools.
    
    * March 14th, 2023: Updated to NET7 Branch and reviews:
     - ToolCursor doesn't needs IConfigurable.
     - ToolCursor uses a dictionary for improved performance.
     - Better performance and structure as overall.
     - Code clean up according to previous reviews.
    
    * review: defer ToolCursor initialization in Load() method

--- Intersect.Editor/Forms/frmOptions.cs
+++ Intersect.Editor/Forms/frmOptions.cs
@@ -28,6 +28,8 @@ namespace Intersect.Editor.Forms
 
             // Load settings for the General tab
             var suppressTilesetWarning = Preferences.LoadPreference("SuppressTextureWarning");
+            var enableCursorSprites = Preferences.LoadPreference("EnableCursorSprites");
+
             if (suppressTilesetWarning == "")
             {
                 chkSuppressTilesetWarning.Checked = false;
@@ -36,6 +38,9 @@ namespace Intersect.Editor.Forms
             {
                 chkSuppressTilesetWarning.Checked = Convert.ToBoolean(suppressTilesetWarning);
             }
+
+            chkCursorSprites.Checked = !string.IsNullOrEmpty(enableCursorSprites) &&
+                                       Convert.ToBoolean(enableCursorSprites);
 
             txtGamePath.Text = Preferences.LoadPreference("ClientPath");
 
@@ -78,6 +83,7 @@ namespace Intersect.Editor.Forms
             Text = Strings.Options.title;
             btnGeneralOptions.Text = Strings.Options.generaltab.ToString(Application.ProductVersion);
             chkSuppressTilesetWarning.Text = Strings.Options.tilesetwarning;
+            chkCursorSprites.Text = Strings.Options.CursorSprites;
             grpClientPath.Text = Strings.Options.pathgroup;
             btnBrowseClient.Text = Strings.Options.browsebtn;
             btnUpdateOptions.Text = Strings.Options.UpdateTab;
@@ -91,6 +97,7 @@ namespace Intersect.Editor.Forms
         private void frmOptions_FormClosing(object sender, FormClosingEventArgs e)
         {
             Preferences.SavePreference("SuppressTextureWarning", chkSuppressTilesetWarning.Checked.ToString());
+            Preferences.SavePreference("EnableCursorSprites", chkCursorSprites.Checked.ToString());
             Preferences.SavePreference("ClientPath", txtGamePath.Text);
             Preferences.SavePreference("PackageUpdateAssets", chkPackageAssets.Checked.ToString());
             Preferences.SavePreference("SoundPackSize", nudSoundBatch.Value.ToString(CultureInfo.InvariantCulture));
