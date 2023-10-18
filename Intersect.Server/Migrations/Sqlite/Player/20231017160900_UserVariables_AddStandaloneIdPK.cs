f95f7b595dece5a38c386e48dd27175a8ced8de4
Robbie Lodico <pandacoder@pm.me>
2023-10-17 23:06:14 -0400

    fix: patch standalone User_Variables.Id for SQLite

--- Intersect.Server/Migrations/Sqlite/Player/20231017160900_UserVariables_AddStandaloneIdPK.cs
+++ Intersect.Server/Migrations/Sqlite/Player/20231017160900_UserVariables_AddStandaloneIdPK.cs
@@ -11,27 +11,23 @@ namespace Intersect.Server.Migrations.Sqlite.Player
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_User_Variables",
-                table: "User_Variables");
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "Id",
-                table: "User_Variables",
-                type: "TEXT",
-                nullable: false,
-                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_User_Variables",
-                table: "User_Variables",
-                column: "Id");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_User_Variables_VariableId_UserId",
-                table: "User_Variables",
-                columns: new[] { "VariableId", "UserId" },
-                unique: true);
+            migrationBuilder.Sql("""
+                                 ALTER TABLE User_Variables ADD COLUMN Id TEXT DEFAULT NULL;
+                                 UPDATE User_Variables SET Id = randomblob(16);
+                                 UPDATE User_Variables SET Id = HEX(Id);
+                                 UPDATE User_Variables SET Id = SUBSTR(Id,1,8)||'-'||SUBSTR(Id,9,4)||'-'||SUBSTR(Id,13,4)||'-'||SUBSTR(Id,17,4)||'-'||SUBSTR(Id,21,12);
+                                 CREATE TABLE ef_temp_User_Variables (
+                                     "Id" TEXT NOT NULL CONSTRAINT "PK_User_Variables" PRIMARY KEY,
+                                     "UserId" TEXT NOT NULL,
+                                     "Value" TEXT NULL,
+                                     "VariableId" TEXT NOT NULL,
+                                     CONSTRAINT "FK_User_Variables_Users_UserId" FOREIGN KEY ("UserId") REFERENCES "Users" ("Id") ON DELETE CASCADE
+                                 );
+                                 CREATE UNIQUE INDEX IF NOT EXISTS IX_User_Variables_VariableId_UserId ON ef_temp_User_Variables (UserId COLLATE BINARY, VariableId COLLATE BINARY);
+                                 INSERT INTO "ef_temp_User_Variables" ("Id", "UserId", "Value", "VariableId") SELECT "Id", "UserId", "Value", "VariableId" FROM "User_Variables";
+                                 DROP TABLE User_Variables;
+                                 ALTER TABLE "ef_temp_User_Variables" RENAME TO "User_Variables";
+            """);
         }
 
         /// <inheritdoc />
