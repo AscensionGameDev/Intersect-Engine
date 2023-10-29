57ab782d4d1e642143928734c1800804c37d7ddc
Arufonsu <17498701+Arufonsu@users.noreply.github.com>
2023-10-29 19:15:12 -0300

    fix: allow diagonal grapple hooks

--- Intersect.Server.Core/Entities/Projectile.cs
+++ Intersect.Server.Core/Entities/Projectile.cs
@@ -299,7 +299,7 @@ namespace Intersect.Server.Entities
                     !spawn.Parent.HasGrappled &&
                     (spawn.X != Owner.X || spawn.Y != Owner.Y))
                 {
-                    if (spawn.Dir <= Direction.Right) //Don't handle directional projectile grapplehooks
+                    if (spawn.Dir != Direction.None)
                     {
                         spawn.Parent.HasGrappled = true;
 
