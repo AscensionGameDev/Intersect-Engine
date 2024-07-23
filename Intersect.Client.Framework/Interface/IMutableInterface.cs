251e5a6220658a279b73a51f39560071d4053881
WeylonSantana <weylonsantanaoff@gmail.com>
2024-07-23 19:03:58 -0300

    refactor mutable interface

--- Intersect.Client.Framework/Interface/IMutableInterface.cs
+++ Intersect.Client.Framework/Interface/IMutableInterface.cs
@@ -1,19 +1,16 @@
-﻿using Intersect.Client.Framework.Gwen.Control;
+using Intersect.Client.Framework.Gwen.Control;
 
 namespace Intersect.Client.Interface;
 
-
 public interface IMutableInterface
 {
-
     List<Base> Children { get; }
 
     TElement Create<TElement>(params object[] parameters) where TElement : Base;
 
-    TElement Find<TElement>(string name = null, bool recurse = false) where TElement : Base;
+    TElement? Find<TElement>(string? name = null, bool recurse = false) where TElement : Base;
 
-    IEnumerable<TElement> FindAll<TElement>(bool recurse = false) where TElement : Base;
+    IEnumerable<TElement?> FindAll<TElement>(bool recurse = false) where TElement : Base;
 
     void Remove<TElement>(TElement element, bool dispose = false) where TElement : Base;
-
 }
