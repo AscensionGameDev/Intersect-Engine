d8e1c48048eeb50e62ee285705c545c776496e7e
WeylonSantana <weylonsantanaoff@gmail.com>
2024-08-01 13:12:44 -0300

    created password utils and unabbreviated variables names

--- Intersect.Client/Interface/Menu/RegisterWindow.cs
+++ Intersect.Client/Interface/Menu/RegisterWindow.cs
@@ -5,6 +5,7 @@ using Intersect.Client.General;
 using Intersect.Client.Interface.Game.Chat;
 using Intersect.Client.Localization;
 using Intersect.Client.Networking;
+using Intersect.Security;
 using Intersect.Utilities;
 
 namespace Intersect.Client.Interface.Menu;
@@ -14,11 +15,11 @@ public partial class RegisterWindow : ImagePanel, IMainMenuWindow
     private readonly MainMenu _mainMenu;
 
     private readonly Label _registrationHeader;
-    private readonly TextBox _txtUsername;
-    private readonly TextBox _txtEmail;
-    private readonly TextBoxPassword _txtPassword;
-    private readonly TextBoxPassword _txtConfirmPassword;
-    private readonly Button _btnRegister;
+    private readonly TextBox _textBoxUsername;
+    private readonly TextBox _textBoxEmail;
+    private readonly TextBoxPassword _textBoxPassword;
+    private readonly TextBoxPassword _textBoxConfirmPassword;
+    private readonly Button _buttonRegister;
 
     //Init
     public RegisterWindow(Canvas parent, MainMenu mainMenu) : base(parent, "RegistrationWindow")
@@ -31,55 +32,36 @@ public partial class RegisterWindow : ImagePanel, IMainMenuWindow
         _registrationHeader.SetText(Strings.Registration.Title);
 
         //Register Username Label/Textbox
-        var _usernameBackground = new ImagePanel(this, "UsernamePanel");
-        _ = new Label(_usernameBackground, "UsernameLabel") { Text = Strings.Registration.Username };
-        _txtUsername = new TextBox(_usernameBackground, "UsernameField")
-        {
-            IsTabable = true,
-        };
-        _txtUsername.SubmitPressed += (s, e) => TryRegister();
+        var _usernameContainer = new ImagePanel(this, "UsernamePanel");
+        _ = new Label(_usernameContainer, "UsernameLabel") { Text = Strings.Registration.Username };
+        _textBoxUsername = new TextBox(_usernameContainer, "UsernameField") { IsTabable = true };
+        _textBoxUsername.SubmitPressed += (s, e) => TryRegister();
 
         //Register Email Label/Textbox
-        var _emailBackground = new ImagePanel(this, "EmailPanel");
-        _ = new Label(_emailBackground, "EmailLabel") { Text = Strings.Registration.Email };
-        _txtEmail = new TextBox(_emailBackground, "EmailField")
-        {
-            IsTabable = true,
-        };
-        _txtEmail.SubmitPressed += (s, e) => TryRegister();
+        var _emailContainer = new ImagePanel(this, "EmailPanel");
+        _ = new Label(_emailContainer, "EmailLabel") { Text = Strings.Registration.Email };
+        _textBoxEmail = new TextBox(_emailContainer, "EmailField") { IsTabable = true };
+        _textBoxEmail.SubmitPressed += (s, e) => TryRegister();
 
         //Register Password Label/Textbox
-        var _passwordBackground = new ImagePanel(this, "Password1Panel");
-        _ = new Label(_passwordBackground, "Password1Label") { Text = Strings.Registration.Password };
-        _txtPassword = new TextBoxPassword(_passwordBackground, "Password1Field")
-        {
-            IsTabable = true,
-        };
-        _txtPassword.SubmitPressed += (s, e) => TryRegister();
+        var _passwordContainer = new ImagePanel(this, "Password1Panel");
+        _ = new Label(_passwordContainer, "Password1Label") { Text = Strings.Registration.Password };
+        _textBoxPassword = new TextBoxPassword(_passwordContainer, "Password1Field") { IsTabable = true };
+        _textBoxPassword.SubmitPressed += (s, e) => TryRegister();
 
         //Register Confirm Password Label/Textbox
-        var _passwordBackground2 = new ImagePanel(this, "Password2Panel");
-        _ = new Label(_passwordBackground2, "Password2Label") { Text = Strings.Registration.ConfirmPassword };
-        _txtConfirmPassword = new TextBoxPassword(_passwordBackground2, "Password2Field")
-        {
-            IsTabable = true,
-        };
-        _txtConfirmPassword.SubmitPressed += (s, e) => TryRegister();
+        var _confirmPasswordContainer = new ImagePanel(this, "Password2Panel");
+        _ = new Label(_confirmPasswordContainer, "Password2Label") { Text = Strings.Registration.ConfirmPassword };
+        _textBoxConfirmPassword = new TextBoxPassword(_confirmPasswordContainer, "Password2Field") { IsTabable = true };
+        _textBoxConfirmPassword.SubmitPressed += (s, e) => TryRegister();
 
         //Register - Send Registration Button
-        _btnRegister = new Button(this, "RegisterButton")
-        {
-            Text = Strings.Registration.Register,
-        };
-        _btnRegister.Clicked += (s, e) => TryRegister();
+        _buttonRegister = new Button(this, "RegisterButton") { Text = Strings.Registration.Register };
+        _buttonRegister.Clicked += (s, e) => TryRegister();
 
         //Register - Back Button
-        var _btnBack = new Button(this, "BackButton")
-        {
-            // IsTabable = true,
-            Text = Strings.Registration.Back,
-        };
-        _btnBack.Clicked += (s, e) =>
+        var _buttonBack = new Button(this, "BackButton") { Text = Strings.Registration.Back };
+        _buttonBack.Clicked += (s, e) =>
         {
             Hide();
             _mainMenu.Show();
@@ -99,16 +81,16 @@ public partial class RegisterWindow : ImagePanel, IMainMenuWindow
         }
 
         // Re-Enable our buttons if we're not waiting for the server anymore with it disabled.
-        if (!Globals.WaitingOnServer && _btnRegister.IsDisabled)
+        if (!Globals.WaitingOnServer && _buttonRegister.IsDisabled)
         {
-            _btnRegister.Enable();
+            _buttonRegister.Enable();
         }
     }
 
     public override void Show()
     {
         base.Show();
-        _txtUsername.Focus();
+        _textBoxUsername.Focus();
     }
 
     void TryRegister()
@@ -124,38 +106,38 @@ public partial class RegisterWindow : ImagePanel, IMainMenuWindow
             return;
         }
 
-        if (!FieldChecking.IsValidUsername(_txtUsername.Text, Strings.Regex.Username))
+        if (!FieldChecking.IsValidUsername(_textBoxUsername.Text, Strings.Regex.Username))
         {
             Interface.ShowError(Strings.Errors.UsernameInvalid);
             return;
         }
 
-        if (!FieldChecking.IsWellformedEmailAddress(_txtEmail.Text, Strings.Regex.Email))
+        if (!FieldChecking.IsWellformedEmailAddress(_textBoxEmail.Text, Strings.Regex.Email))
         {
             Interface.ShowError(Strings.Registration.EmailInvalid);
             return;
         }
 
-        if (!FieldChecking.IsValidPassword(_txtPassword.Text, Strings.Regex.Password))
+        if (!FieldChecking.IsValidPassword(_textBoxPassword.Text, Strings.Regex.Password))
         {
             Interface.ShowError(Strings.Errors.PasswordInvalid);
             return;
         }
 
-        if (_txtPassword.Text != _txtConfirmPassword.Text)
+        if (_textBoxPassword.Text != _textBoxConfirmPassword.Text)
         {
             Interface.ShowError(Strings.Registration.PasswordMismatch);
             return;
         }
 
         PacketSender.SendCreateAccount(
-            _txtUsername.Text,
-            LoginWindow.ComputePasswordHash(_txtPassword.Text.Trim()),
-            _txtEmail.Text
+            _textBoxUsername.Text,
+            PasswordUtils.ComputePasswordHash(_textBoxPassword.Text.Trim()),
+            _textBoxEmail.Text
         );
 
         Globals.WaitingOnServer = true;
-        _btnRegister.Disable();
+        _buttonRegister.Disable();
         ChatboxMsg.ClearMessages();
         Hide();
     }
