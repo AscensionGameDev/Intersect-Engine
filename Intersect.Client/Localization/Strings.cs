0472c7fd315f73bd2f0878a37bd72c09ceaa908e
WeylonSantana <63019821+WeylonSantana@users.noreply.github.com>
2024-08-04 12:20:03 -0300

    chore: refactor guild window (#2371)

--- Intersect.Client/Localization/Strings.cs
+++ Intersect.Client/Localization/Strings.cs
@@ -1102,6 +1102,9 @@ public static partial class Strings
     public partial struct Guilds
     {
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public static LocalizedString Add = @"+";
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public static LocalizedString Bank = @"{00} Guild Bank";
 
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
