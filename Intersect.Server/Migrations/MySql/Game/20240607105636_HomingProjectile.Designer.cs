40bdf8b64e5a35972f38102c7b93f7b067fc9925
Robbie Lodico <pandacoder@pm.me>
2024-08-25 17:45:38 -0400

    fix: Missing MySQL Migration for PR #2263

--- Intersect.Server/Migrations/MySql/Game/20240607105636_HomingProjectile.Designer.cs
+++ Intersect.Server/Migrations/MySql/Game/20240607105636_HomingProjectile.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Intersect.Server.Database.GameData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Intersect.Server.Migrations.MySql.Game
 {
     [DbContext(typeof(MySqlGameContext))]
-    partial class MySqlGameContextModelSnapshot : ModelSnapshot
+    [Migration("20240607105636_HomingProjectile")]
+    partial class HomingProjectile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1536,8 +1539,8 @@ namespace Intersect.Server.Migrations.MySql.Game
                             b1.Property<byte>("Type")
                                 .HasColumnType("tinyint unsigned");
 
-                            b1.Property<long>("Value")
-                                .HasColumnType("bigint");
+                            b1.Property<int>("Value")
+                                .HasColumnType("int");
 
                             b1.HasKey("ItemBaseId");
 
