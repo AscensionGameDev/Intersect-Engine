97efa904423306e0862ea1b54f9220ae58b64f02
Robbie Lodico <pandacoder@pm.me>
2024-08-25 17:43:49 -0400

    fix: Missing MySQL Migration for PR #2208

---
+++ Intersect.Server/Migrations/MySql/Game/20240330154744_ItemEventTriggerMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intersect.Server.Migrations.MySql.Game
+{
+    /// <inheritdoc />
+    public partial class ItemEventTriggerMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EventTriggers",
+                table: "Items",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventTriggers",
+                table: "Items");
+        }
+    }
+}
