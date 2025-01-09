4e8d035084ad2e53733f348c8c510af97050fb3b
Robbie Lodico <pandacoder@pm.me>
2025-01-09 20:16:17 +0100

    fix: corrected weird parameter error by switching from struct to class

--- Intersect.Server/Web/RestApi/Payloads/PagingInfo.cs
+++ Intersect.Server/Web/RestApi/Payloads/PagingInfo.cs
@@ -1,6 +1,7 @@
 ﻿namespace Intersect.Server.Web.RestApi.Payloads;
 
-public struct PagingInfo()
+// TODO: Struct, right now there's an exception when it's a struct and I'm not sure how to fix it
+public class PagingInfo
 {
     public const int MaxPageSize = 100;
     public const int MinPageSize = 5;
@@ -8,5 +9,5 @@ public struct PagingInfo()
 
     public int Page { get; set; } = 0;
 
-    public int Count { get; set; } = 10;
+    public int Count { get; set; } = DefaultPageSize;
 }
