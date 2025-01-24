d80640b7f2bd25fb41bc283006e94a3abf3e1058
Robbie Lodico <pandacoder@pm.me>
2025-01-24 01:28:02 +0100

    CombatOptions convert fields to properties

--- Framework/Intersect.Framework.Core/Config/CombatOptions.cs
+++ Framework/Intersect.Framework.Core/Config/CombatOptions.cs
@@ -2,77 +2,77 @@
 
 public partial class CombatOptions
 {
-    public int BlockingSlow = 30; //Slow when moving with a shield. Default 30%
+    public int BlockingSlow { get; set; } = 30; //Slow when moving with a shield. Default 30%
 
-    public int CombatTime = 10000; //10 seconds
+    public int CombatTime { get; set; } = 10000; //10 seconds
 
-    public int MaxAttackRate = 200; //5 attacks per second
+    public int MaxAttackRate { get; set; } = 200; //5 attacks per second
 
-    public int MaxDashSpeed = 200;
+    public int MaxDashSpeed { get; set; } = 200;
 
-    public int MinAttackRate = 500; //2 attacks per second
+    public int MinAttackRate { get; set; } = 500; //2 attacks per second
 
     //Combat
-    public int RegenTime = 3000; //3 seconds
+    public int RegenTime { get; set; } = 3000; //3 seconds
 
-    public bool EnableCombatChatMessages = false; // Enables or disables combat chat messages.
+    public bool EnableCombatChatMessages { get; set; } = false; // Enables or disables combat chat messages.
 
     //Spells
 
     /// <summary>
     /// If enabled this allows spell casts to stop/be canceled if the player tries to move around (WASD)
     /// </summary>
-    public bool MovementCancelsCast = false;
+    public bool MovementCancelsCast { get; set; } = false;
 
     // Cooldowns
 
     /// <summary>
     /// Configures whether cooldowns within cooldown groups should match.
     /// </summary>
-    public bool MatchGroupCooldowns = true;
+    public bool MatchGroupCooldowns { get; set; } = true;
 
     /// <summary>
     /// Only used when <seealso cref="MatchGroupCooldowns"/> is enabled!
     /// Configures whether cooldowns are being matched to the highest cooldown within a cooldown group when true, or are matched to the current item or spell being used when false.
     /// </summary>
-    public bool MatchGroupCooldownHighest = true;
+    public bool MatchGroupCooldownHighest { get; set; } = true;
 
     /// <summary>
     /// Only used when <seealso cref="MatchGroupCooldowns"/> is enabled!
     /// Configures whether cooldown groups between items and spells are shared.
     /// </summary>
-    public bool LinkSpellAndItemCooldowns = true;
+    public bool LinkSpellAndItemCooldowns { get; set; } = true;
 
     /// <summary>
     /// Configures whether or not using a spell or item should trigger a global cooldown.
     /// </summary>
-    public bool EnableGlobalCooldowns = false;
+    public bool EnableGlobalCooldowns { get; set; } = false;
 
     /// <summary>
     /// Configures the duration (in milliseconds) which the global cooldown lasts after each ability.
     /// Only used when <seealso cref="EnableGlobalCooldowns"/> is enabled!
     /// </summary>
-    public int GlobalCooldownDuration = 1500;
+    public int GlobalCooldownDuration { get; set; } = 1500;
 
     /// <summary>
     /// Configures the maximum distance a target is allowed to be from the player when auto targetting.
     /// </summary>
-    public int MaxPlayerAutoTargetRadius = 15;
+    public int MaxPlayerAutoTargetRadius { get; set; } = 15;
 
     /// <summary>
     /// If enabled this allows regenerate vitals in combat
     /// </summary>
-    public bool RegenVitalsInCombat = false;
+    public bool RegenVitalsInCombat { get; set; } = false;
 
     /// <summary>
     /// If enabled, this allows entities to turn around while casting
     /// </summary>
-    public bool EnableTurnAroundWhileCasting = false;
+    public bool EnableTurnAroundWhileCasting { get; set; } = false;
 
     /// <summary>
     /// If enabled, the target window will be shown to players whenever they target an entity
     /// </summary>
-    public bool EnableTargetWindow = true;
+    public bool EnableTargetWindow { get; set; } = true;
 
     /// <summary>
     /// If enabled, this makes it so a player casting a friendly spell on a hostile target instead casts the spell upon themselves
