6ca23b5cdba26e12ba7608d06ff46d9c82b625c9
Robbie Lodico <pandacoder@pm.me>
2025-01-24 01:27:47 +0100

    ChatOptions convert fields to properties

--- Framework/Intersect.Framework.Core/Config/ChatOptions.cs
+++ Framework/Intersect.Framework.Core/Config/ChatOptions.cs
@@ -5,20 +5,20 @@ public partial class ChatOptions
     /// <summary>
     /// The maximum length of a chat message in characters.
     /// </summary>
-    public int MaxChatLength = 120;
+    public int MaxChatLength { get; set; } = 120;
 
     /// <summary>
     /// The minimum amount of time (in milliseconds) between sending chat messages.
     /// </summary>
-    public int MinIntervalBetweenChats = 400;
+    public int MinIntervalBetweenChats { get; set; } = 400;
 
     /// <summary>
     /// Is the client allowed to show in-game banners for announcements made?
     /// </summary>
-    public bool ShowAnnouncementBanners = true;
+    public bool ShowAnnouncementBanners { get; set; } = true;
 
     /// <summary>
     /// The time (in milliseconds) the announcement banners should display, if enabled.
     /// </summary>
-    public int AnnouncementDisplayDuration = 5000;
+    public int AnnouncementDisplayDuration { get; set; } = 5000;
 }
