e3d063f5deb533446c344fb0d5bec11387236b4e
Robbie Lodico <pandacoder@pm.me>
2025-01-24 01:29:48 +0100

    LootOptions convert fields to properties, revise documentation

--- Framework/Intersect.Framework.Core/Config/LootOptions.cs
+++ Framework/Intersect.Framework.Core/Config/LootOptions.cs
@@ -8,42 +8,42 @@ public partial class LootOptions
     /// <summary>
     /// Defines how long (in ms) loot will be available for picking up on the map.
     /// </summary>
-    public int ItemDespawnTime = 15000;
+    public int ItemDespawnTime { get; set; } = 15000;
 
     /// <summary>
     /// Defines how long (in ms) an item drop will be ''owned'' by a player and their party.
     /// </summary>
-    public int ItemOwnershipTime = 5000;
+    public int ItemOwnershipTime { get; set; } = 5000;
 
     /// <summary>
     /// Defines whether players can see items they do not ''own'' on the map.
     /// </summary>
-    public bool ShowUnownedItems = false;
+    public bool ShowUnownedItems { get; set; } = false;
 
     /// <summary>
-    /// Defines whether or not spawning items on the map from anything but the map attribute should consolidate it into one item.
+    /// Defines whether spawning items on the map from anything but the map attribute should consolidate it into one item.
     /// When false, will drop multiple items with a quantity of 1, rather than a single item with a higher quantity for non-stackable items.
     /// </summary>
-    public bool ConsolidateMapDrops = true;
+    public bool ConsolidateMapDrops { get; set; } = true;
 
     /// <summary>
     /// Configures whether the loot window feature of the client is allowed to be active.
-    /// Note, there's no such setting on the client.. So enabling it here enables it there.
+    /// Note, there's no such setting on the client so enabling it here enables it there.
     /// </summary>
-    public bool EnableLootWindow = true;
+    public bool EnableLootWindow { get; set; } = true;
 
     /// <summary>
     /// Configures the maximum amount of items to be displayed on the client's Loot Window.
     /// </summary>
-    public byte MaximumLootWindowItems = 10;
+    public byte MaximumLootWindowItems { get; set; } = 10;
 
     /// <summary>
     /// Configures the maximum amount of tiles around you to search for loot with the Loot Window.
     /// </summary>
-    public byte MaximumLootWindowDistance = 3;
+    public byte MaximumLootWindowDistance { get; set; } = 3;
 
     /// <summary>
-    /// When killing an npc which spawns individualized loot for all of it's attackers, include loot for all their party members, even if they didn't participate (deal damage) to the npc
+    /// When killing an NPC which spawns individualized loot for all of its attackers, include loot for all their party members, even if they didn't participate (deal damage) to the npc
     /// </summary>
     public bool IndividualizedLootAutoIncludePartyMembers { get; set; } = false;
 }
