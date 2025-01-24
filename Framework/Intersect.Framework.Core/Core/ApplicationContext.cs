b6cf6dc476837dd22f2ba102813942ffb6acd379
Robbie Lodico <pandacoder@pm.me>
2025-01-24 15:29:28 +0100

    add ApplicationContext.CurrentContext

--- Framework/Intersect.Framework.Core/Core/ApplicationContext.cs
+++ Framework/Intersect.Framework.Core/Core/ApplicationContext.cs
@@ -3,4 +3,9 @@ namespace Intersect.Core;
 public static class ApplicationContext
 {
     public static readonly AsyncLocal<IApplicationContext?> Context = new();
+
+    public static IApplicationContext CurrentContext => Context.Value ??
+                                                        throw new InvalidOperationException(
+                                                            "No context has been created for this call stack."
+                                                        );
 }
