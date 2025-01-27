28905cf040f961435617e86d7d2fd8e7c3aa900a
Robbie Lodico <pandacoder@pm.me>
2025-01-27 20:59:11 +0100

    disable server settings access because it's not complete

--- Intersect.Server/Web/Pages/Developer/Index.cshtml.cs
+++ Intersect.Server/Web/Pages/Developer/Index.cshtml.cs
@@ -11,7 +11,7 @@ public class DeveloperIndexModel : PageModel
             "ServerSettings",
             "/Developer/ServerSettings",
             () => "STUBServerSettings",
-            context => true
+            context => false
         ),
     ];
 
