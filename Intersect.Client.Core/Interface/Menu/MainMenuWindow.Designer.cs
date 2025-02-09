9539c8b0ff2d300294696f8a2221b727658ce5de
Robbie Lodico <pandacoder@pm.me>
2025-02-09 12:20:21 +0100

    fix MainMenuWindow alignments

--- Intersect.Client.Core/Interface/Menu/MainMenuWindow.Designer.cs
+++ Intersect.Client.Core/Interface/Menu/MainMenuWindow.Designer.cs
@@ -29,61 +29,38 @@ public partial class MainMenuWindow
             _buttonExit,
         }.Where(button => button.IsVisible).ToArray();
 
-        const int spacerX = 0;
-        const int spacerY = 4;
         const int defaultWidth = 87;
         const int defaultHeight = 154;
 
-        var innerWidth = (spacerX + defaultWidth) * visibleButtons.Length - spacerX;
-        var innerHeight = spacerY * 2 + defaultHeight;
-
-        SetSize(
-            innerWidth + InnerPanelPadding.Left + InnerPanelPadding.Right,
-            innerHeight + TitleBarBounds.Bottom + InnerPanelPadding.Top + InnerPanelPadding.Bottom
+        Size = new Point(
+            defaultWidth * visibleButtons.Length + InnerPanelPadding.Left + InnerPanelPadding.Right,
+            defaultHeight + TitleBarBounds.Bottom + InnerPanelPadding.Top + InnerPanelPadding.Bottom
         );
 
         AddAlignment(Framework.Gwen.Alignments.Center);
-        AlignmentPadding = new Padding(0, 40, 0, 0);
+        AlignmentTranslation = new Point(0, 40);
         ProcessAlignments();
 
         TitleLabel.TextColor = Color.White;
         TitleLabel.FontName = "sourcesansproblack";
-        TitleLabel.FontSize = 12;
-        TitleLabel.Padding = new Padding(8, 4, 8, 4);
-        TitleLabel.SizeToContents();
+        TitleLabel.FontSize = 14;
 
         Titlebar.SetBounds(0, 0, Width, TitleLabel.Height);
 
-        var x = InnerPanelPadding.Left;
         foreach (var button in visibleButtons)
         {
-            button.SetBounds(x, spacerY, defaultWidth, defaultHeight);
-            x += defaultWidth + spacerX;
-
+            button.Size = new Point(defaultWidth, defaultHeight);
             button.FontName = "sourcesansproblack";
             button.FontSize = 12;
-            button.Padding = new Padding(0, 8, 0, 0);
-            button.TextColor = Color.White;
-            button.TextColorOverride = Color.White;
+            button.Padding = new Padding(0, 24, 0, 0);
+            button.Dock = Pos.Left;
             button.SetHoverSound("octave-tap-resonant.wav");
 
             var buttonName = button.Name;
-            button.SetStateTexture(
-                $"mainmenu{buttonName}.png",
-                ComponentState.Normal
-            );
-            button.SetStateTexture(
-                $"mainmenu{buttonName}_clicked.png",
-                ComponentState.Active
-            );
-            button.SetStateTexture(
-                $"mainmenu{buttonName}_disabled.png",
-                ComponentState.Disabled
-            );
-            button.SetStateTexture(
-                $"mainmenu{buttonName}_hovered.png",
-                ComponentState.Hovered
-            );
+            button.SetStateTexture($"mainmenu{buttonName}.png", ComponentState.Normal);
+            button.SetStateTexture($"mainmenu{buttonName}_clicked.png", ComponentState.Active);
+            button.SetStateTexture($"mainmenu{buttonName}_disabled.png", ComponentState.Disabled);
+            button.SetStateTexture($"mainmenu{buttonName}_hovered.png", ComponentState.Hovered);
         }
 
         LoadJsonUi(GameContentManager.UI.Menu, Graphics.Renderer.GetResolutionString());
