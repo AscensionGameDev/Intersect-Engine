c5a77b86b5fe24cadd373aca4abdccefe330e9b8
Derek Gooding <djgooding@live.com>
2025-02-15 10:16:32 -0500

    Fix spacing for button text and Main title.
    Used the params from Gwen.Control.Titlebar, which is the default for MainWindow : Window so it would look similar.

--- Intersect.Client.Core/Interface/Game/EscapeMenu.cs
+++ Intersect.Client.Core/Interface/Game/EscapeMenu.cs
@@ -1,5 +1,6 @@
 using Intersect.Client.Core;
 using Intersect.Client.Framework.File_Management;
+using Intersect.Client.Framework.Gwen;
 using Intersect.Client.Framework.Gwen.Control;
 using Intersect.Client.Framework.Gwen.Control.EventArguments;
 using Intersect.Client.General;
@@ -15,6 +16,8 @@ public partial class EscapeMenu : ImagePanel
     private readonly Button _buttonCharacterSelect;
     private readonly Panel _versionPanel;
 
+    private readonly Framework.Graphics.GameFont _font;
+
     public EscapeMenu(Canvas gameCanvas, Func<SettingsWindow> settingsWindowProvider) : base(gameCanvas, nameof(EscapeMenu))
     {
         _settingsWindowProvider = settingsWindowProvider;
@@ -24,6 +27,8 @@ public partial class EscapeMenu : ImagePanel
         Width = gameCanvas.Width;
         Height = gameCanvas.Height;
 
+        _font = GameContentManager.Current!.GetFont("sourcesansproblack", 12)!;
+
         // Create the container
         var container = new ImagePanel(this, nameof(EscapeMenu));
 
@@ -31,45 +36,38 @@ public partial class EscapeMenu : ImagePanel
         _ = new Label(container, "TitleLabel")
         {
             Text = Strings.EscapeMenu.Title,
+            AutoSizeToContents = false,
+            Dock = Pos.Fill | Pos.CenterV,
+            MouseInputEnabled = false,
+            Padding = new Padding(8, 2, 0, 0),
+            TextAlign = Pos.Left | Pos.Top,
+            TextColor = Skin.Colors.Window.TitleInactive,
+            Font = _font,
+            FontSize = 18,
         };
 
-        var buttonSettings = new Button(container, "SettingsButton")
-        {
-            IsTabable = true,
-            Text = Strings.EscapeMenu.Settings,
-        };
+        var buttonSettings = new Button(container, "SettingsButton");
+        InitializeButton(buttonSettings, Strings.EscapeMenu.Settings);
         buttonSettings.Clicked += (s, e) => OpenSettingsWindow(true);
 
         // Character Select Button
-        _buttonCharacterSelect = new Button(container, "CharacterSelectButton")
-        {
-            IsTabable = true,
-            Text = Strings.EscapeMenu.CharacterSelect,
-        };
+        _buttonCharacterSelect = new Button(container, "CharacterSelectButton");
+        InitializeButton(_buttonCharacterSelect, Strings.EscapeMenu.CharacterSelect);
         _buttonCharacterSelect.Clicked += _buttonCharacterSelect_Clicked;
 
         // Logout Button
-        var buttonLogout = new Button(container, "LogoutButton")
-        {
-            IsTabable = true,
-            Text = Strings.EscapeMenu.Logout,
-        };
+        var buttonLogout = new Button(container, "LogoutButton");
+        InitializeButton(buttonLogout, Strings.EscapeMenu.Logout);
         buttonLogout.Clicked += buttonLogout_Clicked;
 
         // Exit to Desktop Button
-        var buttonQuit = new Button(container, "ExitToDesktopButton")
-        {
-            IsTabable = true,
-            Text = Strings.EscapeMenu.ExitToDesktop,
-        };
+        var buttonQuit = new Button(container, "ExitToDesktopButton");
+        InitializeButton(buttonQuit, Strings.EscapeMenu.ExitToDesktop);
         buttonQuit.Clicked += buttonQuit_Clicked;
 
         // Close Button
-        var buttonContinue = new Button(container, "CloseButton")
-        {
-            IsTabable = true,
-            Text = Strings.EscapeMenu.Close,
-        };
+        var buttonContinue = new Button(container, "CloseButton");
+        InitializeButton(buttonContinue, Strings.EscapeMenu.Close);
         buttonContinue.Clicked += (s, e) => Hide();
 
         container.LoadJsonUi(GameContentManager.UI.InGame, Graphics.Renderer?.GetResolutionString());
@@ -80,6 +78,18 @@ public partial class EscapeMenu : ImagePanel
         }
 
         _versionPanel = new VersionPanel(this, name: nameof(_versionPanel));
+    }
+
+    private void InitializeButton(Button button, string text)
+    {
+        _ = new Label(button)
+        {
+            Text = text,
+            Font = _font,
+            Padding = new Padding(0, 20, 0, 0),
+            Alignment = [ Alignments.Center, Alignments.CenterV ],
+        };
+        button.IsTabable = true;
     }
 
     public override void Invalidate()
