01425b8f709eb7458bfb90aa1ce2acc3267a3e00
Derek Gooding <djgooding@live.com>
2025-02-17 09:16:41 -0500

    Create EscapeMenuWindow
    With accompanied Designer file
    
    Signed-off-by: Derek Gooding <djgooding@live.com>

---
+++ Intersect.Client.Core/Interface/Menu/EscapeMenuWindow.Designer.cs
@@ -0,0 +1,48 @@
+using Intersect.Client.Framework.Gwen;
+using Intersect.Client.Framework.Gwen.Control;
+
+namespace Intersect.Client.Interface.Menu;
+
+public partial class EscapeMenuWindow
+{
+    protected override void EnsureInitialized()
+    {
+        var canvas = Canvas ?? throw new InvalidOperationException($"Not attached to a {nameof(Canvas)}");
+
+        Button[] visibleButtons = new[]
+        {
+            _settings,
+            _charselect,
+            _logout,
+            _exitdesktop,
+            _closemenu,
+        }.Where(button => button.IsVisible).ToArray();
+
+        const int defaultWidth = 97;
+        const int defaultHeight = 154;
+
+        Size = new Point(
+            defaultWidth * visibleButtons.Length + InnerPanelPadding.Left + InnerPanelPadding.Right,
+            defaultHeight + TitleBarBounds.Bottom + InnerPanelPadding.Top + InnerPanelPadding.Bottom
+        );
+
+        Titlebar.MouseInputEnabled = false;
+        TitleLabel.FontSize = 14;
+        TitleLabel.TextColorOverride = Color.White;
+
+        foreach (var button in visibleButtons)
+        {
+            button.Size = new Point(defaultWidth, defaultHeight);
+            button.FontName = "sourcesansproblack";
+            button.FontSize = 12;
+            button.Padding = new Padding(0, 24, 0, 0);
+            button.Dock = Pos.Left;
+
+            var buttonName = button.Name;
+            button.SetStateTexture(ComponentState.Normal, $"ingame{buttonName}.png");
+            button.SetStateTexture(ComponentState.Active, $"ingame{buttonName}_clicked.png");
+            button.SetStateTexture(ComponentState.Disabled, $"ingame{buttonName}_disabled.png");
+            button.SetStateTexture(ComponentState.Hovered, $"ingame{buttonName}_hovered.png");
+        }
+    }
+}
---
+++ Intersect.Client.Core/Interface/Menu/EscapeMenuWindow.cs
@@ -0,0 +1,211 @@
+using Intersect.Client.Core;
+using Intersect.Client.Framework.Gwen;
+using Intersect.Client.Framework.Gwen.Control;
+using Intersect.Client.Framework.Gwen.Control.EventArguments;
+using Intersect.Client.General;
+using Intersect.Client.Interface.Shared;
+using Intersect.Client.Localization;
+using Intersect.Utilities;
+
+namespace Intersect.Client.Interface.Menu;
+
+public partial class EscapeMenuWindow : Window
+{
+    private readonly Button _settings;
+    private readonly Button _charselect;
+    private readonly Button _logout;
+    private readonly Button _exitdesktop;
+    private readonly Button _closemenu;
+
+    private readonly Panel _versionPanel;
+
+    private readonly Func<SettingsWindow> _settingsWindowProvider;
+
+    public EscapeMenuWindow(Canvas canvas, Func<SettingsWindow> settingsWindowProvider)
+        : base(canvas, Strings.EscapeMenu.Title, false,
+            $"{nameof(EscapeMenuWindow)}_{(ClientContext.IsSinglePlayer ? "singleplayer" : "online")}")
+    {
+        _settingsWindowProvider = settingsWindowProvider;
+
+        Alignment = [Alignments.Center];
+
+        IsResizable = false;
+        Padding = Padding.Zero;
+        InnerPanelPadding = new Padding(8);
+        Titlebar.MouseInputEnabled = false;
+
+        _settings = new Button(this, nameof(_settings))
+        {
+            IsTabable = true,
+            Text = Strings.EscapeMenu.Settings,
+        };
+        _settings.Clicked += (s, e) => OpenSettingsWindow(true);
+
+        _charselect = new Button(this, nameof(_charselect))
+        {
+            IsTabable = true,
+            Text = Strings.EscapeMenu.CharacterSelect,
+        };
+        _charselect.Clicked += _buttonCharacterSelect_Clicked;
+
+        _logout = new Button(this, nameof(_logout))
+        {
+            IsTabable = true,
+            Text = Strings.EscapeMenu.Logout,
+        };
+        _logout.Clicked += buttonLogout_Clicked;
+
+        _exitdesktop = new Button(this, nameof(_exitdesktop))
+        {
+            IsTabable = true,
+            Text = Strings.EscapeMenu.ExitToDesktop,
+        };
+        _exitdesktop.Clicked += buttonQuit_Clicked;
+
+        _closemenu = new Button(this, nameof(_closemenu))
+        {
+            IsTabable = true,
+            Text = Strings.EscapeMenu.Close,
+        };
+        _closemenu.Clicked += (s, e) => Hide();
+
+        if (!string.IsNullOrEmpty(Strings.MainMenu.SettingsTooltip))
+        {
+            _settings.SetToolTipText(Strings.MainMenu.SettingsTooltip);
+        }
+
+        if (Options.Instance.Player.MaxCharacters <= 1)
+        {
+            _charselect.IsDisabled = true;
+        }
+
+        _versionPanel = new VersionPanel(canvas, name: nameof(_versionPanel));
+    }
+
+    public override void Invalidate()
+    {
+        if (IsHidden)
+        {
+            RemoveModal();
+        }
+        else
+        {
+            MakeModal(true);
+        }
+
+        base.Invalidate();
+        if (Interface.GameUi?.GameCanvas != null)
+        {
+            Interface.GameUi.GameCanvas.MouseInputEnabled = IsVisible;
+        }
+    }
+
+    /// <inheritdoc />
+    public override void ToggleHidden()
+    {
+        var settingsWindow = _settingsWindowProvider();
+        if (settingsWindow.IsVisible)
+        {
+            return;
+        }
+
+        base.ToggleHidden();
+    }
+
+    public void Update()
+    {
+        if (!IsHidden)
+        {
+            BringToFront();
+        }
+
+        _charselect.IsDisabled = Globals.Me?.CombatTimer > Timing.Global.Milliseconds;
+    }
+
+    public void OpenSettingsWindow(bool returnToMenu = false)
+    {
+        var settingsWindow = _settingsWindowProvider();
+        settingsWindow.Show(returnToMenu ? this : null);
+        Hide();
+    }
+
+    private void _buttonCharacterSelect_Clicked(Base sender, MouseButtonState arguments)
+    {
+        ToggleHidden();
+        if (Globals.Me?.CombatTimer > Timing.Global.Milliseconds)
+        {
+            ShowCombatWarning();
+        }
+        else
+        {
+            LogoutToCharacterSelect(null, null);
+        }
+    }
+
+    private void LogoutToCharacterSelect(object? sender, EventArgs? e)
+    {
+        if (Globals.Me != null)
+        {
+            Globals.Me.CombatTimer = 0;
+        }
+
+        Main.Logout(true);
+    }
+
+    private void buttonLogout_Clicked(Base sender, MouseButtonState arguments)
+    {
+        ToggleHidden();
+        if (Globals.Me?.CombatTimer > Timing.Global.Milliseconds)
+        {
+            ShowCombatWarning();
+        }
+        else
+        {
+            LogoutToMainMenu(null, null);
+        }
+    }
+
+    private void LogoutToMainMenu(object? sender, EventArgs? e)
+    {
+        if (Globals.Me != null)
+        {
+            Globals.Me.CombatTimer = 0;
+        }
+
+        Main.Logout(false);
+    }
+
+    private void buttonQuit_Clicked(Base sender, MouseButtonState arguments)
+    {
+        ToggleHidden();
+        if (Globals.Me?.CombatTimer > Timing.Global.Milliseconds)
+        {
+            ShowCombatWarning();
+        }
+        else
+        {
+            ExitToDesktop(null, null);
+        }
+    }
+
+    private void ShowCombatWarning()
+    {
+        AlertWindow.Open(
+            Strings.Combat.WarningCharacterSelect,
+            Strings.Combat.WarningTitle,
+            AlertType.Warning,
+            handleSubmit: LogoutToCharacterSelect,
+            inputType: InputType.YesNo
+        );
+    }
+
+    private void ExitToDesktop(object? sender, EventArgs? e)
+    {
+        if (Globals.Me != null)
+        {
+            Globals.Me.CombatTimer = 0;
+        }
+
+        Globals.IsRunning = false;
+    }
+}
