726625109b6e170954f0c5dcb1debaf2b3274572
WeylonSantana <weylonsantanaoff@gmail.com>
2025-02-24 01:18:02 -0300

    clean up shop window/item

--- Intersect.Client.Core/Interface/Game/Shop/ShopWindow.cs
+++ Intersect.Client.Core/Interface/Game/Shop/ShopWindow.cs
@@ -1,129 +1,57 @@
-﻿using Intersect.Client.Core;
+using Intersect.Client.Core;
 using Intersect.Client.Framework.File_Management;
 using Intersect.Client.Framework.Gwen.Control;
 using Intersect.Client.General;
 using Intersect.Client.Localization;
-using Intersect.GameObjects;
 
 namespace Intersect.Client.Interface.Game.Shop;
 
-
-public partial class ShopWindow
+public partial class ShopWindow : Window
 {
+    private readonly List<ShopItem> _items = [];
+    private readonly ScrollControl _slotContainer;
 
-    private static int sItemXPadding = 4;
-
-    private static int sItemYPadding = 4;
-
-    public List<ShopItem> Items = new List<ShopItem>();
-
-    private ScrollControl mItemContainer;
-
-    //Controls
-    private WindowControl mShopWindow;
-
-    // Context menu
-    private Framework.Gwen.Control.Menu mContextMenu;
-
-    private MenuItem mBuyContextItem;
-
-    //Init
-    public ShopWindow(Canvas gameCanvas)
+    public ShopWindow(Canvas gameCanvas) : base(gameCanvas, Globals.GameShop?.Name ?? Strings.Shop.Title, false, nameof(ShopWindow))
     {
-        mShopWindow = new WindowControl(gameCanvas, Globals.GameShop.Name, false, "ShopWindow");
-        mShopWindow.DisableResizing();
-        Interface.InputBlockingComponents.Add(mShopWindow);
+        DisableResizing();
+        Interface.InputBlockingComponents.Add(this);
 
-        mItemContainer = new ScrollControl(mShopWindow, "ItemContainer");
-        mItemContainer.EnableScroll(false, true);
+        _slotContainer = new ScrollControl(this, "ItemContainer");
+        _slotContainer.EnableScroll(false, true);
+    }
 
-        mShopWindow.LoadJsonUi(GameContentManager.UI.InGame, Graphics.Renderer.GetResolutionString());
-
+    protected override void EnsureInitialized()
+    {
+        LoadJsonUi(GameContentManager.UI.InGame, Graphics.Renderer.GetResolutionString());
         InitItemContainer();
-
-        // Generate our context menu with basic options.
-        mContextMenu = new Framework.Gwen.Control.Menu(gameCanvas, "ShopContextMenu");
-        mContextMenu.IsHidden = true;
-        mContextMenu.IconMarginDisabled = true;
-        //TODO: Is this a memory leak?
-        mContextMenu.ClearChildren();
-        mBuyContextItem = mContextMenu.AddItem(Strings.ShopContextMenu.Buy);
-        mBuyContextItem.Clicked += MBuyContextItem_Clicked;
-        mContextMenu.LoadJsonUi(GameContentManager.UI.InGame, Graphics.Renderer.GetResolutionString());
-    }
-
-    public void OpenContextMenu(int slot)
-    {
-        var item = ItemBase.Get(Globals.GameShop.SellingItems[slot].ItemId);
-
-        // No point showing a menu for blank space.
-        if (item == null)
-        {
-            return;
-        }
-
-        mBuyContextItem.SetText(Strings.ShopContextMenu.Buy.ToString(item.Name));
-
-        // Set our spell slot as userdata for future reference.
-        mContextMenu.UserData = slot;
-
-        mContextMenu.SizeToChildren();
-        mContextMenu.Open(Framework.Gwen.Pos.None);
-    }
-
-    private void MBuyContextItem_Clicked(Base sender, Framework.Gwen.Control.EventArguments.MouseButtonState arguments)
-    {
-        var slot = (int) sender.Parent.UserData;
-        Globals.Me.TryBuyItem(slot);
-    }
-
-    //Location
-    public int X => mShopWindow.X;
-
-    public int Y => mShopWindow.Y;
-
-    public void Close()
-    {
-        mContextMenu?.Close();
-        mShopWindow.Close();
-    }
-
-    public bool IsVisible()
-    {
-        return !mShopWindow.IsHidden;
-    }
-
-    public void Hide()
-    {
-        mShopWindow.IsHidden = true;
     }
 
     private void InitItemContainer()
     {
+        if (Globals.GameShop == default || Globals.GameShop.SellingItems.Count == 0)
+        {
+            return;
+        }
+
         for (var i = 0; i < Globals.GameShop.SellingItems.Count; i++)
         {
-            Items.Add(new ShopItem(this, i));
-            Items[i].Container = new ImagePanel(mItemContainer, "ShopItem");
-            Items[i].Setup();
+            _items.Add(new ShopItem(this, _slotContainer, i));
 
-            Items[i].Container.LoadJsonUi(GameContentManager.UI.InGame, Graphics.Renderer.GetResolutionString());
+            var xPadding = _items[i].Margin.Left + _items[i].Margin.Right;
+            var yPadding = _items[i].Margin.Top + _items[i].Margin.Bottom;
 
-            Items[i].LoadItem();
+            var itemWidthWithPadding = _items[i].Width + xPadding;
+            var itemHeightWithPadding = _items[i].Height + yPadding;
 
-            var xPadding = Items[i].Container.Margin.Left + Items[i].Container.Margin.Right;
-            var yPadding = Items[i].Container.Margin.Top + Items[i].Container.Margin.Bottom;
-            Items[i]
-                .Container.SetPosition(
-                    i %
-                    (mItemContainer.Width / (Items[i].Container.Width + xPadding)) *
-                    (Items[i].Container.Width + xPadding) +
-                    xPadding,
-                    i /
-                    (mItemContainer.Width / (Items[i].Container.Width + xPadding)) *
-                    (Items[i].Container.Height + yPadding) +
-                    yPadding
-                );
+            var itemsPerRow = _slotContainer.Width / itemWidthWithPadding;
+
+            var column = i % itemsPerRow;
+            var row = i / itemsPerRow;
+
+            var xPosition = column * itemWidthWithPadding + xPadding;
+            var yPosition = row * itemHeightWithPadding + yPadding;
+
+            _items[i].SetPosition(xPosition, yPosition);
         }
     }
-
 }
