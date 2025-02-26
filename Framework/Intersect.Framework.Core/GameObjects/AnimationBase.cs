a45402401243da5ac7d006ea781ffef55640c6ac
Robbie Lodico <pandacoder@pm.me>
2025-02-26 19:45:02 +0100

    split AnimationLayer into separate file

--- Framework/Intersect.Framework.Core/GameObjects/AnimationBase.cs
+++ Framework/Intersect.Framework.Core/GameObjects/AnimationBase.cs
@@ -1,49 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using Intersect.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Intersect.Models;
 using Newtonsoft.Json;
 
 namespace Intersect.GameObjects;
-
-[Owned]
-public partial class AnimationLayer
-{
-    public AnimationLayer()
-    {
-        Lights = new LightBase[FrameCount];
-
-        for (var frame = 0; frame < FrameCount; ++frame)
-        {
-            Lights[frame] = new LightBase();
-        }
-    }
-
-    public string Sprite { get; set; } = string.Empty;
-
-    public int FrameCount { get; set; } = 1;
-
-    public int XFrames { get; set; } = 1;
-
-    public int YFrames { get; set; } = 1;
-
-    public int FrameSpeed { get; set; } = 100;
-
-    public int LoopCount { get; set; }
-
-    public bool DisableRotations { get; set; }
-
-    public bool AlternateRenderLayer { get; set; }
-
-    [JsonIgnore]
-    public string Light
-    {
-        get => JsonConvert.SerializeObject(Lights);
-        set => Lights = JsonConvert.DeserializeObject<LightBase[]>(value);
-    }
-
-    [NotMapped]
-    public LightBase[] Lights { get; set; }
-}
 
 public partial class AnimationBase : DatabaseObject<AnimationBase>, IFolderable
 {
---
+++ Framework/Intersect.Framework.Core/GameObjects/Animations/AnimationLayer.cs
@@ -0,0 +1,45 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Intersect.GameObjects;
+
+[Owned]
+public partial class AnimationLayer
+{
+    public AnimationLayer()
+    {
+        Lights = new LightBase[FrameCount];
+
+        for (var frame = 0; frame < FrameCount; ++frame)
+        {
+            Lights[frame] = new LightBase();
+        }
+    }
+
+    public string Sprite { get; set; } = string.Empty;
+
+    public int FrameCount { get; set; } = 1;
+
+    public int XFrames { get; set; } = 1;
+
+    public int YFrames { get; set; } = 1;
+
+    public int FrameSpeed { get; set; } = 100;
+
+    public int LoopCount { get; set; }
+
+    public bool DisableRotations { get; set; }
+
+    public bool AlternateRenderLayer { get; set; }
+
+    [JsonIgnore]
+    public string Light
+    {
+        get => JsonConvert.SerializeObject(Lights);
+        set => Lights = JsonConvert.DeserializeObject<LightBase[]>(value);
+    }
+
+    [NotMapped]
+    public LightBase[] Lights { get; set; }
+}
