a76dd0298a9ef7429e58bbb32b12ecb32694a409
Robbie Lodico <pandacoder@pm.me>
2025-02-27 11:57:15 -0500

    move ConditionListsSerializer to its own file

---
+++ Framework/Intersect.Framework.Core/GameObjects/Conditions/ConditionListsSerializer.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Intersect.GameObjects.Conditions;
+
+public partial class ConditionListsSerializer : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return true;
+    }
+
+    public override object ReadJson(
+        JsonReader reader,
+        Type objectType,
+        object existingValue,
+        JsonSerializer serializer
+    )
+    {
+        var jsonObject = JObject.Load(reader);
+        var properties = jsonObject.Properties().ToList();
+        var lists = existingValue != null ? (ConditionLists) existingValue : new ConditionLists();
+        lists.Load((string) properties[0].Value);
+
+        return lists;
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        writer.WriteStartObject();
+        writer.WritePropertyName("Lists");
+        serializer.Serialize(writer, ((ConditionLists)value).Data());
+        writer.WriteEndObject();
+    }
+}
