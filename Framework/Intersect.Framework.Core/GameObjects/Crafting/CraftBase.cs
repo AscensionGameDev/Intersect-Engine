09aaf5c3b1c9ff0384e7bd51ae39b4d28ce45d80
Robbie Lodico <pandacoder@pm.me>
2025-02-27 11:37:55 -0500

    separate CraftIngredient into its own file

--- Framework/Intersect.Framework.Core/GameObjects/Crafting/CraftBase.cs
+++ Framework/Intersect.Framework.Core/GameObjects/Crafting/CraftBase.cs
@@ -73,21 +73,3 @@ public partial class CraftBase : DatabaseObject<CraftBase>, IFolderable
         set => CraftingRequirements.Load(value ?? "[]");
     }
 }
-
-public partial class CraftIngredient
-{
-    public Guid ItemId { get; set; }
-
-    public int Quantity { get; set; }
-
-    public CraftIngredient(Guid itemId, int quantity)
-    {
-        ItemId = itemId;
-        Quantity = quantity;
-    }
-
-    public ItemBase GetItem()
-    {
-        return ItemBase.Get(ItemId);
-    }
-}
---
+++ Framework/Intersect.Framework.Core/GameObjects/Crafting/CraftIngredient.cs
@@ -0,0 +1,19 @@
+﻿namespace Intersect.GameObjects.Crafting;
+
+public partial class CraftIngredient
+{
+    public Guid ItemId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public CraftIngredient(Guid itemId, int quantity)
+    {
+        ItemId = itemId;
+        Quantity = quantity;
+    }
+
+    public ItemBase GetItem()
+    {
+        return ItemBase.Get(ItemId);
+    }
+}
