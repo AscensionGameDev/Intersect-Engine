83a4bd452207fc3f049fd8e63308ca3ab06a217a
Robbie Lodico <pandacoder@pm.me>
2025-02-27 19:16:54 +0100

    handle namespace renames in `Intersect Core` and `Intersect.Framework.Core`

--- Framework/Intersect.Framework.Core/Serialization/IntersectTypeSerializationBinder.cs
+++ Framework/Intersect.Framework.Core/Serialization/IntersectTypeSerializationBinder.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Intersect.Framework.Reflection;
 using Newtonsoft.Json.Serialization;
 
 namespace Intersect.Framework.Core.Serialization;
@@ -7,11 +10,44 @@ public sealed class IntersectTypeSerializationBinder : ISerializationBinder
 {
     private readonly DefaultSerializationBinder _defaultSerializationBinder = new();
 
-    public Type BindToType(string assemblyName, string typeName)
+    public Type BindToType(string assemblyName, string qualifiedTypeName)
     {
-        return assemblyName.StartsWith("Intersect")
-            ? Type.GetType(typeName)
-            : _defaultSerializationBinder.BindToType(assemblyName, typeName);
+        if (!assemblyName.StartsWith("Intersect"))
+        {
+            return _defaultSerializationBinder.BindToType(assemblyName, qualifiedTypeName);
+        }
+
+        var type = Type.GetType(qualifiedTypeName);
+        if (type is not null)
+        {
+            return type;
+        }
+
+        switch (assemblyName)
+        {
+            case "Intersect Core":
+            case "Intersect.Framework.Core":
+                break;
+            default:
+                throw new NotImplementedException($"{nameof(assemblyName)}=\"{assemblyName}\", {nameof(qualifiedTypeName)}=\"{qualifiedTypeName}\"");
+        }
+
+        var typeNameSegments = qualifiedTypeName.Split('.');
+        var typeName = qualifiedTypeName.Split('.').Last();
+
+        var assembly = Assembly.Load("Intersect.Framework.Core");
+        var types = assembly.GetTypes();
+        var matchingTypes = types.Where(t => typeName.Equals(t.Name)).ToArray();
+
+        return matchingTypes.Length switch
+        {
+            1 => matchingTypes.First(),
+            < 1 => throw new InvalidOperationException($"No matching types for {qualifiedTypeName}"),
+            > 2 => throw new InvalidOperationException(
+                $"Multiple matching types for {qualifiedTypeName}:\n{string.Join("\n", matchingTypes.Select(t => $"\t{t.GetName(qualified: true)}"))}"
+            ),
+            _ => throw new UnreachableException("There should be no uncovered value"),
+        };
     }
 
     public void BindToName(
