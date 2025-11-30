775a428bb061a984ec0c40dbb01e0b3e811c4fce
JC Snider <jcsnider3@gmail.com>
2025-11-30 16:29:36 -0500

    Determine default keybinds from default_settings.json. Fallback on hardcoded values.

--- Intersect.Client.Framework/Input/BuiltinControlsProvider.cs
+++ Intersect.Client.Framework/Input/BuiltinControlsProvider.cs
@@ -5,51 +5,7 @@ namespace Intersect.Client.Framework.Input;
 
 internal sealed class BuiltinControlsProvider : IControlsProvider
 {
-    private readonly Dictionary<Control, ControlMapping> _defaultMappings = new()
-    {
-        { Control.MoveUp, new ControlMapping(new ControlBinding(Keys.None, Keys.Up), new ControlBinding(Keys.None, Keys.W)) },
-        { Control.MoveDown, new ControlMapping(new ControlBinding(Keys.None, Keys.Down), new ControlBinding(Keys.None, Keys.S)) },
-        { Control.MoveLeft, new ControlMapping(new ControlBinding(Keys.None, Keys.Left), new ControlBinding(Keys.None, Keys.A)) },
-        { Control.MoveRight, new ControlMapping(new ControlBinding(Keys.None, Keys.Right), new ControlBinding(Keys.None, Keys.D)) },
-        { Control.AttackInteract, new ControlMapping(new ControlBinding(Keys.None, Keys.E), new ControlBinding(Keys.None, Keys.LButton)) },
-        { Control.Block, new ControlMapping(new ControlBinding(Keys.None, Keys.Q), new ControlBinding(Keys.None, Keys.RButton)) },
-        { Control.AutoTarget, new ControlMapping(new ControlBinding(Keys.None, Keys.Tab), ControlBinding.Default) },
-        { Control.HoldToSoftRetargetOnSelfCast, new ControlMapping(new ControlBinding(Keys.None, Keys.LMenu), ControlBinding.Default) },
-        { Control.ToggleAutoSoftRetargetOnSelfCast, new ControlMapping(ControlBinding.Default, ControlBinding.Default) },
-        { Control.PickUp, new ControlMapping(new ControlBinding(Keys.None, Keys.Space), ControlBinding.Default) },
-        { Control.Enter, new ControlMapping(new ControlBinding(Keys.None, Keys.Enter), ControlBinding.Default) },
-        { Control.Screenshot, new ControlMapping(new ControlBinding(Keys.None, Keys.F12), ControlBinding.Default) },
-        { Control.OpenMenu, new ControlMapping(new ControlBinding(Keys.None, Keys.Escape), ControlBinding.Default) },
-        { Control.OpenInventory, new ControlMapping(new ControlBinding(Keys.None, Keys.I), ControlBinding.Default) },
-        { Control.OpenQuests, new ControlMapping(new ControlBinding(Keys.None, Keys.L), ControlBinding.Default) },
-        { Control.OpenCharacterInfo, new ControlMapping(new ControlBinding(Keys.None, Keys.C), ControlBinding.Default) },
-        { Control.OpenParties, new ControlMapping(new ControlBinding(Keys.None, Keys.P), ControlBinding.Default) },
-        { Control.OpenSpells, new ControlMapping(new ControlBinding(Keys.None, Keys.K), ControlBinding.Default) },
-        { Control.OpenFriends, new ControlMapping(new ControlBinding(Keys.None, Keys.F), ControlBinding.Default) },
-        { Control.OpenGuild, new ControlMapping(new ControlBinding(Keys.None, Keys.G), ControlBinding.Default) },
-        { Control.OpenSettings, new ControlMapping(new ControlBinding(Keys.None, Keys.O), ControlBinding.Default) },
-        { Control.OpenDebugger, new ControlMapping(new ControlBinding(Keys.None, Keys.F2), ControlBinding.Default) },
-        { Control.OpenAdminPanel, new ControlMapping(new ControlBinding(Keys.None, Keys.Insert), ControlBinding.Default) },
-        { Control.ToggleGui, new ControlMapping(new ControlBinding(Keys.None, Keys.F11), ControlBinding.Default) },
-        { Control.TurnAround, new ControlMapping(new ControlBinding(Keys.None, Keys.Control), ControlBinding.Default) },
-        { Control.ToggleZoomIn, new ControlMapping(ControlBinding.Default, ControlBinding.Default) },
-        { Control.ToggleZoomOut, new ControlMapping(ControlBinding.Default, ControlBinding.Default) },
-        { Control.HoldToZoomIn, new ControlMapping(ControlBinding.Default, ControlBinding.Default) },
-        { Control.HoldToZoomOut, new ControlMapping(ControlBinding.Default, ControlBinding.Default) },
-        { Control.ToggleFullscreen, new ControlMapping(new ControlBinding(Keys.Alt, Keys.Enter), ControlBinding.Default) },
-
-        // Hotkeys should be at the end of the list
-        { Control.Hotkey1, new ControlMapping(new ControlBinding(Keys.None, Keys.D1), ControlBinding.Default) },
-        { Control.Hotkey2, new ControlMapping(new ControlBinding(Keys.None, Keys.D2), ControlBinding.Default) },
-        { Control.Hotkey3, new ControlMapping(new ControlBinding(Keys.None, Keys.D3), ControlBinding.Default) },
-        { Control.Hotkey4, new ControlMapping(new ControlBinding(Keys.None, Keys.D4), ControlBinding.Default) },
-        { Control.Hotkey5, new ControlMapping(new ControlBinding(Keys.None, Keys.D5), ControlBinding.Default) },
-        { Control.Hotkey6, new ControlMapping(new ControlBinding(Keys.None, Keys.D6), ControlBinding.Default) },
-        { Control.Hotkey7, new ControlMapping(new ControlBinding(Keys.None, Keys.D7), ControlBinding.Default) },
-        { Control.Hotkey8, new ControlMapping(new ControlBinding(Keys.None, Keys.D8), ControlBinding.Default) },
-        { Control.Hotkey9, new ControlMapping(new ControlBinding(Keys.None, Keys.D9), ControlBinding.Default) },
-        { Control.Hotkey10, new ControlMapping(new ControlBinding(Keys.None, Keys.D0), ControlBinding.Default) },
-    };
+    private readonly Dictionary<Control, ControlMapping> _defaultMappings = LoadDefaultMappings();
 
     public Control[] Controls { get; } = Enum.GetValues<Control>().Where(control => control.IsValid()).ToArray();
 
@@ -67,5 +23,79 @@ internal sealed class BuiltinControlsProvider : IControlsProvider
 
     public void ReloadFromOptions(Options? options)
     {
+        // Builtin controls don't change based on options
+        // Defaults are loaded once from default_settings.json at initialization
+    }
+
+    /// <summary>
+    /// Loads default mappings by starting with hardcoded defaults and then merging overrides from default_settings.json if present.
+    /// </summary>
+    private static Dictionary<Control, ControlMapping> LoadDefaultMappings()
+    {
+        // Start with all hardcoded defaults
+        var defaults = GetHardcodedDefaults();
+
+        // Load file overrides
+        var fileDefaults = DefaultSettingsLoader.LoadDefaultsFromFile(defaults.Keys);
+
+        // Merge: file overrides hardcoded defaults
+        foreach (var (control, mapping) in fileDefaults)
+        {
+            defaults[control] = mapping;
+        }
+
+        return defaults;
+    }
+
+    /// <summary>
+    /// Returns the hardcoded default control mappings.
+    /// </summary>
+    private static Dictionary<Control, ControlMapping> GetHardcodedDefaults()
+    {
+        return new Dictionary<Control, ControlMapping>
+        {
+            { Control.MoveUp, new ControlMapping(new ControlBinding(Keys.None, Keys.Up), new ControlBinding(Keys.None, Keys.W)) },
+            { Control.MoveDown, new ControlMapping(new ControlBinding(Keys.None, Keys.Down), new ControlBinding(Keys.None, Keys.S)) },
+            { Control.MoveLeft, new ControlMapping(new ControlBinding(Keys.None, Keys.Left), new ControlBinding(Keys.None, Keys.A)) },
+            { Control.MoveRight, new ControlMapping(new ControlBinding(Keys.None, Keys.Right), new ControlBinding(Keys.None, Keys.D)) },
+            { Control.AttackInteract, new ControlMapping(new ControlBinding(Keys.None, Keys.E), new ControlBinding(Keys.None, Keys.LButton)) },
+            { Control.Block, new ControlMapping(new ControlBinding(Keys.None, Keys.Q), new ControlBinding(Keys.None, Keys.RButton)) },
+            { Control.AutoTarget, new ControlMapping(new ControlBinding(Keys.None, Keys.Tab), ControlBinding.Default) },
+            { Control.HoldToSoftRetargetOnSelfCast, new ControlMapping(new ControlBinding(Keys.None, Keys.LMenu), ControlBinding.Default) },
+            { Control.ToggleAutoSoftRetargetOnSelfCast, new ControlMapping(ControlBinding.Default, ControlBinding.Default) },
+            { Control.PickUp, new ControlMapping(new ControlBinding(Keys.None, Keys.Space), ControlBinding.Default) },
+            { Control.Enter, new ControlMapping(new ControlBinding(Keys.None, Keys.Enter), ControlBinding.Default) },
+            { Control.Screenshot, new ControlMapping(new ControlBinding(Keys.None, Keys.F12), ControlBinding.Default) },
+            { Control.OpenMenu, new ControlMapping(new ControlBinding(Keys.None, Keys.Escape), ControlBinding.Default) },
+            { Control.OpenInventory, new ControlMapping(new ControlBinding(Keys.None, Keys.I), ControlBinding.Default) },
+            { Control.OpenQuests, new ControlMapping(new ControlBinding(Keys.None, Keys.L), ControlBinding.Default) },
+            { Control.OpenCharacterInfo, new ControlMapping(new ControlBinding(Keys.None, Keys.C), ControlBinding.Default) },
+            { Control.OpenParties, new ControlMapping(new ControlBinding(Keys.None, Keys.P), ControlBinding.Default) },
+            { Control.OpenSpells, new ControlMapping(new ControlBinding(Keys.None, Keys.K), ControlBinding.Default) },
+            { Control.OpenFriends, new ControlMapping(new ControlBinding(Keys.None, Keys.F), ControlBinding.Default) },
+            { Control.OpenGuild, new ControlMapping(new ControlBinding(Keys.None, Keys.G), ControlBinding.Default) },
+            { Control.OpenSettings, new ControlMapping(new ControlBinding(Keys.None, Keys.O), ControlBinding.Default) },
+            { Control.OpenDebugger, new ControlMapping(new ControlBinding(Keys.None, Keys.F2), ControlBinding.Default) },
+            { Control.OpenAdminPanel, new ControlMapping(new ControlBinding(Keys.None, Keys.Insert), ControlBinding.Default) },
+            { Control.ToggleGui, new ControlMapping(new ControlBinding(Keys.None, Keys.F11), ControlBinding.Default) },
+            { Control.TurnAround, new ControlMapping(new ControlBinding(Keys.None, Keys.Control), ControlBinding.Default) },
+            { Control.ToggleZoomIn, new ControlMapping(ControlBinding.Default, ControlBinding.Default) },
+            { Control.ToggleZoomOut, new ControlMapping(ControlBinding.Default, ControlBinding.Default) },
+            { Control.HoldToZoomIn, new ControlMapping(ControlBinding.Default, ControlBinding.Default) },
+            { Control.HoldToZoomOut, new ControlMapping(ControlBinding.Default, ControlBinding.Default) },
+            { Control.ToggleFullscreen, new ControlMapping(new ControlBinding(Keys.Alt, Keys.Enter), ControlBinding.Default) },
+
+            // Hotkeys should be at the end of the list
+            { Control.Hotkey1, new ControlMapping(new ControlBinding(Keys.None, Keys.D1), ControlBinding.Default) },
+            { Control.Hotkey2, new ControlMapping(new ControlBinding(Keys.None, Keys.D2), ControlBinding.Default) },
+            { Control.Hotkey3, new ControlMapping(new ControlBinding(Keys.None, Keys.D3), ControlBinding.Default) },
+            { Control.Hotkey4, new ControlMapping(new ControlBinding(Keys.None, Keys.D4), ControlBinding.Default) },
+            { Control.Hotkey5, new ControlMapping(new ControlBinding(Keys.None, Keys.D5), ControlBinding.Default) },
+            { Control.Hotkey6, new ControlMapping(new ControlBinding(Keys.None, Keys.D6), ControlBinding.Default) },
+            { Control.Hotkey7, new ControlMapping(new ControlBinding(Keys.None, Keys.D7), ControlBinding.Default) },
+            { Control.Hotkey8, new ControlMapping(new ControlBinding(Keys.None, Keys.D8), ControlBinding.Default) },
+            { Control.Hotkey9, new ControlMapping(new ControlBinding(Keys.None, Keys.D9), ControlBinding.Default) },
+            { Control.Hotkey10, new ControlMapping(new ControlBinding(Keys.None, Keys.D0), ControlBinding.Default) },
+        };
     }
 }
--- Intersect.Client.Framework/Input/HotkeyControlsProvider.cs
+++ Intersect.Client.Framework/Input/HotkeyControlsProvider.cs
@@ -5,14 +5,21 @@ namespace Intersect.Client.Framework.Input;
 
 internal sealed class HotkeyControlsProvider : IControlsProvider
 {
+    private Dictionary<Control, ControlMapping>? _defaultMappings = null;
+
     public HotkeyControlsProvider() => Controls = ControlsFromOptions(Options.Instance);
 
     public Control[] Controls { get; private set; }
 
     public bool TryGetDefaultMapping(Control control, [NotNullWhen(true)] out ControlMapping? defaultMapping)
     {
-        defaultMapping = null;
-        return false;
+        // Lazy-load on first call
+        if (_defaultMappings == null)
+        {
+            _defaultMappings = DefaultSettingsLoader.LoadDefaultsFromFile(Controls);
+        }
+
+        return _defaultMappings.TryGetValue(control, out defaultMapping);
     }
 
     private static Control[] ControlsFromOptions(Options? options) =>
@@ -21,5 +28,9 @@ internal sealed class HotkeyControlsProvider : IControlsProvider
             .Select(hotkeyValue => Control.HotkeyOffset + hotkeyValue)
             .ToArray();
 
-    public void ReloadFromOptions(Options? options) => Controls = ControlsFromOptions(options);
+    public void ReloadFromOptions(Options? options)
+    {
+        Controls = ControlsFromOptions(options);
+        _defaultMappings = null; // Force reload with new hotbar slot count
+    }
 }
