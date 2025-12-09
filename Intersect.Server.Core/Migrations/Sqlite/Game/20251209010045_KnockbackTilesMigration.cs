922eecf3b13f6c859af992da230a5fb8e04d11fe
Kamus <kamusvalenzuela2@gmail.com>
2025-12-09 09:03:58 -0300

    Migration files (this was helped by AI (Gemini)) so maybe its all wrong, use with caution (works for me but with a initial bug) all damages and values of the spells are reseted by default when runs by first time

---
+++ Intersect.Server.Core/Migrations/Sqlite/Game/20251209010045_KnockbackTilesMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intersect.Server.Migrations.Sqlite.Game
+{
+    /// <inheritdoc />
+    public partial class KnockbackTilesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Combat_KnockbackTiles",
+                table: "Spells",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Combat_KnockbackTiles",
+                table: "Spells");
+        }
+    }
+}
