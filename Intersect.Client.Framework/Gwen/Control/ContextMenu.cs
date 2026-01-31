a73e5c715e17185c18509787dc71ef624dc72018
Aru <17498701+Arufonsu@users.noreply.github.com>
2026-01-31 15:18:24 -0300

    fix: combobox/menu items width (#2799)
    fixes #2553
    
    Signed-off-by: Arufonsu <17498701+Arufonsu@users.noreply.github.com>

--- Intersect.Client.Framework/Gwen/Control/ContextMenu.cs
+++ Intersect.Client.Framework/Gwen/Control/ContextMenu.cs
@@ -1,23 +1,3 @@
 namespace Intersect.Client.Framework.Gwen.Control;
 
-public class ContextMenu : Menu
-{
-    public ContextMenu(Base parent, string? name = default) : base(parent, name)
-    {
-
-    }
-
-    protected override void OnPositioningBeforeOpen()
-    {
-        base.OnPositioningBeforeOpen();
-
-        SizeToChildren(recursive: true);
-    }
-
-    protected override void OnOpen()
-    {
-        base.OnOpen();
-
-        PostLayout.Enqueue(contextMenu => contextMenu.SizeToChildren(recursive: true), this);
-    }
-}
+public class ContextMenu(Base parent, string? name = default) : Menu(parent, name);
